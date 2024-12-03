4b49f24115106b107f0f1b09722c7bf70959e26c
Giang-Davy <davy.dangvu@gmail.com>
2024-12-03 10:40:52 +0000

    2V2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
 def matrix_shape(matrix):
-    ligne = len(matrix)
-    colonne = len(matrix[0])
-    return [ligne, colonne]
+    # Vérifier si la matrice est une liste de listes (2D)
+    if isinstance(matrix[0], list):
+        ligne = len(matrix)
+        colonne = len(matrix[0])
+        return [ligne, colonne]
+    else:
+        return [len(matrix), 1]  # Si c'est une matrice 1D (liste d'entiers)
