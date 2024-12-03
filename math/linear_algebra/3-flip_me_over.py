82ffdbe6d26d013a5f72aff1f6f7772d17b5e69b
Giang-Davy <davy.dangvu@gmail.com>
2024-12-03 14:25:55 +0000

    3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""fonction"""
+
+
+def matrix_transpose(matrix):
+    """init"""
+
+    transposed = []
+    for i in range(len(matrix[0])):  # Itère sur les colonnes
+        colo = []  # Liste pour la nouvelle ligne
+        for row in matrix:  # Parcourt chaque e originale
+            colo.append(row[i])  # Ajoute l'é i à la nouvelle ligne
+        transposed.append(colo)  # Ajoute la le à la matrice finale
+    return transposed
