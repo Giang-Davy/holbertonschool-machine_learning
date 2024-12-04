e8d84ba38d8c3565e81cc6c9bdc094917dc46570
Giang-Davy <davy.dangvu@gmail.com>
2024-12-04 10:43:30 +0000

    7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""fonction"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Args: ff
+    Returns: ff
+    Exemple: ff
+    """
+    if axis == 0:
+        add = mat1 + mat2
+        return add
+
+    else:
+        if len(mat1) != len(mat2):
+            return None
+        new_matrix = []
+        for i in range(len(mat1)):
+            new = mat1[i] + mat2[i]
+            new_matrix.append(new)
+        return new_matrix
