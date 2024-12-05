ae9203579465969290233277b1bcd80af6061b34
Giang-Davy <davy.dangvu@gmail.com>
2024-12-05 11:09:30 +0000

    12V3

--- math/linear_algebra/backup.py
+++ math/linear_algebra/backup.py
@@ -7,8 +7,8 @@ def np_elementwise(mat1, mat2):
     Args: ff
     Returns: ff
     """
-    add = mat1 + mat2
-    sub = mat1 - mat2
-    mul = mat1 * mat2
-    div = mat1 / mat2
+    add = np.add(mat1, mat2)
+    sub = np.subtract(mat1, mat2)
+    mul = np.multiply(mat1, mat2)
+    div = np.divide(mat1, mat2)
     return add, sub, mul, div
