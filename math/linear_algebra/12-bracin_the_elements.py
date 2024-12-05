cb82e90cbd8246fc00ecfde79913e10397f077c6
Giang-Davy <davy.dangvu@gmail.com>
2024-12-05 11:03:06 +0000

    12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,12 +2,16 @@
 """fonction"""
 
 
+import numpy as np
+
+
 def np_elementwise(mat1, mat2):
     """
+    Args: ff
     Returns: ff
     """
     add = np.add(mat1, mat2)
     sub = np.subtract(mat1, mat2)
-    mul = np.matmul(mat1, mat2)
+    mul = np.multiply(mat1, mat2)
     div = np.divide(mat1, mat2)
     return add, sub, mul, div
