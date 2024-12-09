0d8b3316ecd2744bbb93e3336169952b4267dc72
Giang-Davy <davy.dangvu@gmail.com>
2024-12-09 14:58:13 +0100

    0V3

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+"""ff"""
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """
+    Args: ff
+
+    Returns: ff
+    """
     # Définir les valeurs de y (cube des nombres de 0 à 10)
     y = np.arange(0, 11) ** 3
     x = np.arange(0, 11)  # Créer les valeurs de x de 0 à 10
@@ -17,4 +26,3 @@ def line():
 
     # Afficher le graphique
     plt.show()
-
