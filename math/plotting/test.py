728f17bfbd48aa77176fed1cfd0817f647836cb3
Giang-Davy <davy.dangvu@gmail.com>
2024-12-09 11:51:52 +0100

    0

---
+++ math/plotting/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+import matplotlib.pyplot as plt
+
+
+# Tracer un graphique
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+plt.plot(x, y)
+
+# Afficher le graphique
+plt.show()
+
