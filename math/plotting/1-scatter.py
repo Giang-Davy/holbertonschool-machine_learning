32ae4caccfd3341bdc181cd974c731966525b726
Giang-Davy <davy.dangvu@gmail.com>
2024-12-09 16:47:39 +0100

    1V7

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Génère un graphique de dispersion de la taille et du poids des hommes"""
+"""Blabla"""
 
 
 import numpy as np
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 def scatter():
     """
-    Génère un graphique de dispersion des données simulées pour la taille et le poids des hommes.
+    ffland
     """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
@@ -16,11 +16,9 @@ def scatter():
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
     plt.figure(figsize=(6.4, 4.8))
-    plt.xlim([50, 90])
-    plt.ylim([150, 210])
     plt.xlabel("Height (in)")
     plt.ylabel("Weight (lbs)")
-    plt.title("Men's Height vs Weight")
+    plt.title("Men\'s Height vs Weight")
     plt.scatter(x, y, color='m', s=10)
     plt.show()
 
