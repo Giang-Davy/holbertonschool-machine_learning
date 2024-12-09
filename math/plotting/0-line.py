463e7e6ebde809100984afb41651f904e5831985
Giang-Davy <davy.dangvu@gmail.com>
2024-12-09 14:31:52 +0100

    0V2

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,9 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-
+    # Définir les valeurs de y (cube des nombres de 0 à 10)
     y = np.arange(0, 11) ** 3
-    plt.figure(figsize=(6.4, 4.8))
+    x = np.arange(0, 11)  # Créer les valeurs de x de 0 à 10
 
-    x = [0, 0, 0, 0]
-    y = [2, 2, 2, 2]
+    plt.figure(figsize=(6.4, 4.8))  # Taille de la figure
+
+    # Tracer y comme une ligne rouge continue
+    plt.plot(x, y, color='r', linestyle='-')
+
+    # Définir la plage de l'axe des x de 0 à 10
+    plt.xlim(0, 10)
+
+    # Afficher le graphique
+    plt.show()
+
