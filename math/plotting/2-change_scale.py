adb92fda89e306e9e8d8e2b840eab0c1d765e58d
Giang-Davy <davy.dangvu@gmail.com>
2024-12-10 11:21:49 +0000

    2V3

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""ffff"""
+"""fffff"""
 
 
 import numpy as np
@@ -7,16 +7,23 @@ import matplotlib.pyplot as plt
 
 
 def change_scale():
-    """blabla"""
-    x = np.arange(0, 28651, 5730)
-    r = np.log(0.5)
-    t = 5730
-    y = np.exp((r / t) * x)
-    plt.figure(figsize=(6.4, 4.8))
-    plt.yscale('log')
-    plt.plot(x, y)
-    plt.xlabel('Time (years)')
-    plt.ylabel('Fraction Remaining')
-    plt.title('Exponential Dacay of C-14')
-    plt.margins(0)
-    plt.show()
+    """
+    fffffff
+    """
+	x = np.arange(0, 28651, 5730)
+	r = np.log(0.5)
+	t = 5730
+	y = np.exp((r / t) * x)
+	plt.figure(figsize=(6.4, 4.8))
+	plt.plot(x, y, marker='o')
+	plt.xlabel("Time (years)")
+	plt.ylabel("Fraction Remaining")
+	plt.title("Exponential Decay of C-14")
+	plt.yscale("log")
+	plt.xlim(0, 28650)
+	plt.grid(True)
+	plt.show()
+
+
+change_scale()
+
