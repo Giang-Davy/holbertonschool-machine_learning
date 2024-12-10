c0fbf97ffc47f94bde4263c093d8caec7f53ac72
Giang-Davy <davy.dangvu@gmail.com>
2024-12-10 15:20:44 +0000

    4

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""fffff"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    ffff
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.ylim(0, 30)
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    # Affichage des graduations tous les 10 sur l'axe des x
+    plt.xticks(np.arange(0, 101, 10))
+    plt.show()
