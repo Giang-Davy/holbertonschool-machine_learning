b46b654213eef961a5672c384f0b43f61eea398e
Giang-Davy <davy.dangvu@gmail.com>
2024-12-12 16:18:46 +0000

    10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""blabla"""
+
+
+def poly_derivative(poly):
+    """
+    Args: ff
+    Returns: ff
+    """
+    if not isinstance(poly, list) or not all(isinstance(c, (int, float)) for c in poly):
+        return None
+    if len(poly) <= 1:
+        return [0]
+    return [i * poly[i] for i in range(1, len(poly))]
+
+
+poly = [5, 3, 0, 1]
+print(poly_derivative(poly))
