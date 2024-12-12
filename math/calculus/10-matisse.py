de353f9530ccd68b717931e90d9cceabea2eb4f3
Giang-Davy <davy.dangvu@gmail.com>
2024-12-12 16:21:59 +0000

    10V3

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,6 +9,8 @@ def poly_derivative(poly):
     """
     if not isinstance(poly, list) or not all(isinstance(c, (int, float)) for c in poly):
         return None
-    if len(poly) <= 1:
+    if len(poly) == 0:
+        return None
+    if len(poly) == 1:
         return [0]
     return [i * poly[i] for i in range(1, len(poly))]
