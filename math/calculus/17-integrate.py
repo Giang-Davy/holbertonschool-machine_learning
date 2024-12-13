c4b19dcaaa9558d023a3142a0540fc953a5673b9
Giang-Davy <davy.dangvu@gmail.com>
2024-12-13 13:43:27 +0000

    17V77777777

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -8,18 +8,16 @@ def poly_integral(poly, C=0):
     """
     if not isinstance(poly, list) or not all(isinstance(j, (int, float)) for j in poly) or not isinstance(C, (int, float)):
         return None
-    if not poly:  # Vérification si poly est vide
-        return None
-    integral_poly = [C]
+    integral_poly = []
+    
+    if C != 0:  # Si C n'est pas zéro, on l'ajoute
+        integral_poly.append(C)
+    
     for i in range(len(poly)):
         new_coeff = poly[i] / (i + 1)
-        if new_coeff == int(new_coeff):
-            new_coeff = int(new_coeff)  # Convertir en entier si possible
-        integral_poly.append(new_coeff)
+        if new_coeff == int(new_coeff):  # Si le coefficient est un entier
+            new_coeff = int(new_coeff)
+        if new_coeff != 0 or integral_poly:  # Si ce n'est pas zéro, ou si c'est le premier terme
+            integral_poly.append(new_coeff)
     
-    # Ajouter des zéros dans la sortie pour correspondre à l'attendu (s'il y a des zéros explicites à insérer)
-    degree = len(poly)
-    for i in range(degree + 1, len(integral_poly)):
-        integral_poly.insert(i, 0)
-
     return integral_poly
