5382b16707f2c17eb93fde1a2ec96a6cdd8012b4
Giang-Davy <davy.dangvu@gmail.com>
2024-12-13 12:27:03 +0000

    17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""blabla"""
+
+
+def poly_integral(poly, C=0):
+    """
+    Args: ff
+    Returns: ff
+    """
+    if not isinstance(poly, list) or not all(
+            isinstance(c, (int, float)) for c in poly):
+        return None
+    integral_poly = [C]
+    for i in range(len(poly)):
+        new_coeff = poly[i] / (i + 1)
+        if new_coeff == int(new_coeff):
+            integral_poly.append(int(new_coeff))
+        else:
+            integral_poly.append(new_coeff)
+    return integral_poly
