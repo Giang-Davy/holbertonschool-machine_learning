167abc50785a4fdc68c65e68fa3c5984acb8cb66
Giang-Davy <davy.dangvu@gmail.com>
2024-12-13 13:46:34 +0000

    17V77777777888888

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -8,22 +8,16 @@ def poly_integral(poly, C=0):
     """
     if not isinstance(poly, list) or not all(isinstance(j, (int, float)) for j in poly) or not isinstance(C, (int, float)):
         return None
-    
-    # Si le polynôme est vide, on retourne une liste vide
     if not poly:
-        return []
-    
-    integral_poly = []
-    
-    # Ajouter C seulement s'il est différent de 0
-    if C != 0 or len(poly) > 1:  # Ajouter C seulement si poly a plus d'un terme
-        integral_poly.append(C)
-    
-    # Calcul de l'intégrale
+        return None
+    integral_poly = [C]
+    if len(poly) == 1:
+        if poly == [0]:
+            return integral_poly
     for i in range(len(poly)):
         new_coeff = poly[i] / (i + 1)
         if new_coeff == int(new_coeff):
-            new_coeff = int(new_coeff)
-        integral_poly.append(new_coeff)
-    
+            integral_poly.append(int(new_coeff))
+        else:
+            integral_poly.append(new_coeff)
     return integral_poly
