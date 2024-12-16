994f75c8b444d82ab5f434b1a6d04a72b9a0081c
Giang-Davy <davy.dangvu@gmail.com>
2024-12-16 14:50:08 +0000

    0V7

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -6,14 +6,22 @@ import numpy as np
 
 
 class Node:
+    """
+    Noeud interne de l'arbre de décision.
+    """
+
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
         """
-        Calcule la profondeur actuel, y compris
-        les feuilles.
+        Initialise un noeud interne.
 
-        Retourne :
-            int : la profondeuud.
+        Args:
+            feature (int, optional): Caractéristique pour la séparation.
+            threshold (float, optional): Seuil de séparation.
+            left_child (Node or Leaf, optional): Sous-arbre gauche.
+            right_child (Node or Leaf, optional): Sous-arbre droit.
+            is_root (bool, optional): Noeud racine (par défaut False).
+            depth (int, optional): Profondeur du noeud (par défaut 0).
         """
         self.feature = feature
         self.threshold = threshold
@@ -25,35 +33,66 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
+        """
+        Calcule la profondeur maximale sous ce noeud.
+
+        Returns:
+            int: Profondeur maximale sous ce noeud.
+        """
         if self.is_leaf:
             return self.depth
-        left_depth = (
-            self.left_child.max_depth_below()
-            if self.left_child
-            else self.depth
-        )
-        right_depth = (
-            self.right_child.max_depth_below()
-            if self.right_child
-            else self.depth
-        )
+        left_depth = (self.left_child.max_depth_below()
+                      if self.left_child else self.depth)
+        right_depth = (self.right_child.max_depth_below()
+                       if self.right_child else self.depth)
         return max(left_depth, right_depth)
 
 
 class Leaf(Node):
+    """
+    Représente une feuille dans un arbre de décision.
+    """
+
     def __init__(self, value, depth=None):
+        """
+        Initialise une feuille.
+
+        Args:
+            value (any): Valeur associée à cette feuille.
+            depth (int, optional): Profondeur de la feuille.
+        """
         super().__init__()
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
     def max_depth_below(self):
+        """
+        Retourne la profondeur de la feuille.
+
+        Returns:
+            int: La profondeur de la feuille.
+        """
         return self.depth
 
 
 class Decision_Tree:
+    """
+    Représente un arbre de décision.
+    """
+
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
+        """
+        Initialise un arbre de décision.
+
+        Args:
+            max_depth (int, optional): Profondeur maximale de l'arbre.
+            min_pop (int, optional): Taille minimale des sous-populations.
+            seed (int, optional): Graine pour le générateur.
+            split_criterion (str, optional): Critère de séparation.
+            root (Node, optional): Racine de l'arbre.
+        """
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -67,4 +106,10 @@ class Decision_Tree:
         self.predict = None
 
     def depth(self):
+        """
+        Calcule la profondeur maximale de l'arbre.
+
+        Returns:
+            int: Profondeur maximale de l'arbre.
+        """
         return self.root.max_depth_below()
