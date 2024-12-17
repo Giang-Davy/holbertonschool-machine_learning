f325d41035eb5270911de747340597e43304c9f0
Giang-Davy <davy.dangvu@gmail.com>
2024-12-17 16:43:28 +0000

    3V2

--- supervised_learning/decision_tree/3-build_decision_tree.py
+++ supervised_learning/decision_tree/3-build_decision_tree.py
@@ -5,6 +5,8 @@ import numpy as np
 
 
 class Node:
+"""FFFFFFF"""
+
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
         """
