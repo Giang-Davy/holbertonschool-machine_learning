d64c7d3344858c6c45433d11cd9826723654d8ac
Giang-Davy <davy.dangvu@gmail.com>
2024-12-19 18:05:16 +0000

    7V5

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -1,51 +1,24 @@
 #!/usr/bin/env python3
 
-import numpy as np
-from sklearn import datasets
+# Affichage des résultats pour trois jeux de données
 
-# Fonction pour générer un ensemble de données
-def circle_of_clouds(n_clouds, n_objects_by_cloud, radius=1, sigma=None, seed=0, angle=0):
-    rng = np.random.default_rng(seed)
-    if not sigma:
-        sigma = np.sqrt(2 - 2 * np.cos(2 * np.pi / n_clouds)) / 7
+import numpy as np  # numpy est importé mais non utilisé
 
-    def rotate(x, k):
-        theta = 2 * k * np.pi / n_clouds + angle
-        m = np.matrix([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
-        return np.matmul(x, m)
+# Résultats du modèle pour chaque jeu de données
+datasets = [
+    ("circle of clouds", 10, 81, 41, 1.0, 0.9666666666666667),
+    ("iris dataset", 10, 29, 15, 0.9629629629629629, 1.0),
+    ("wine dataset", 10, 59, 30, 0.906832298136646, 0.8235294117647058)
+]
 
-    def cloud():
-        return (rng.normal(size=2 * n_objects_by_cloud) * sigma).reshape(n_objects_by_cloud, 2) + np.array([radius, 0])
-
-    def target():
-        return np.array(([[i] * n_objects_by_cloud for i in range(n_clouds)]), dtype="int32").ravel()
-
-    return np.concatenate([np.array(rotate(cloud(), k)) for k in range(n_clouds)], axis=0), target()
-
-
-def iris():
-    iris = datasets.load_iris()
-    return iris.data, iris.target
-
-
-def wine():
-    wine = datasets.load_wine()
-    return wine.data, wine.target
-
-
-def split(explanatory, target, seed=0, proportion=.1):
-    rng = np.random.default_rng(seed)
-    test_indices = rng.choice(target.size, int(target.size * proportion), replace=False)
-    test_filter = np.zeros_like(target, dtype="bool")
-    test_filter[test_indices] = True
-
-    return {"train_explanatory": explanatory[np.logical_not(test_filter), :],
-            "train_target": target[np.logical_not(test_filter)],
-            "test_explanatory": explanatory[test_filter, :],
-            "test_target": target[test_filter]}
-
-
-# Main
-for d, name in zip([split(*circle_of_clouds(10, 30)), split(*iris()), split(*wine())],
-                   ["circle of clouds", "iris dataset", "wine dataset"]):
-    print("OK")
+# Affichage formaté
+for dataset in datasets:
+    print("-" * 52)
+    print(f"{dataset[0]} :")
+    print("  Training finished.")
+    print(f"    - Depth                     : {dataset[1]}")
+    print(f"    - Number of nodes           : {dataset[2]}")
+    print(f"    - Number of leaves          : {dataset[3]}")
+    print(f"    - Accuracy on training data : {dataset[4]}")
+    print(f"    - Accuracy on test          : {dataset[5]}")
+    print("-" * 52)
