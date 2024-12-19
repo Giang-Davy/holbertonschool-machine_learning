c6ab5cb1401d86130c22832697dcebe2f9f530de
Giang-Davy <davy.dangvu@gmail.com>
2024-12-19 15:04:50 +0000

    6V10

--- supervised_learning/decision_tree/6-build_decision_tree.py
+++ supervised_learning/decision_tree/6-build_decision_tree.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+"""fonction"""
+
 
 import numpy as np
 
 
 class Node:
-    # Classe représentant un nœud dans un arbre de décision
+    """
+    Nœud dans un arbre de décision.
+    """
+
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
+        """
+        Initialise un nœud.
+        """
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -15,14 +23,16 @@ class Node:
         self.is_root = is_root
         self.sub_population = None
         self.depth = depth
-        # Initialisation des bornes
-        self.lower = {}  # Les bornes inférieures pour chaque feature
-        self.upper = {}  # Les bornes supérieures pour chaque feature
+        self.lower = {}
+        self.upper = {}
         if feature is not None:
-            self.lower[feature] = -100  # Valeur initiale arbitraire
-            self.upper[feature] = 100   # Valeur initiale arbitraire
+            self.lower[feature] = -100
+            self.upper[feature] = 100
 
     def __str__(self):
+        """
+        Représentation en chaîne du nœud.
+        """
         node_type = "root" if self.is_root else "node"
         details = f"{node_type} [feature={self.feature},"
         details += f"threshold={self.threshold}]\n"
@@ -38,21 +48,36 @@ class Node:
 
 
 class Leaf(Node):
-    # Classe représentant une feuille dans l'arbre de décision
+    """
+    Feuille dans un arbre de décision.
+    """
+
     def __init__(self, value, depth=None):
+        """
+        Initialise une feuille.
+        """
         super().__init__()
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
     def __str__(self):
+        """
+        Représentation en chaîne de la feuille.
+        """
         return f"leaf [value={self.value}]"
 
 
 class Decision_Tree:
-    # Classe représentant un arbre de décision
+    """
+    Arbre de décision.
+    """
+
     def __init__(self, max_depth=10, min_pop=1,
                  seed=0, split_criterion="random", root=None):
+        """
+        Initialise l'arbre de décision.
+        """
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -60,12 +85,21 @@ class Decision_Tree:
             self.root = Node(is_root=True)
 
     def __str__(self):
+        """
+        Représentation en chaîne de l'arbre de décision.
+        """
         return self.root.__str__()
 
     def update_predict(self):
+        """
+        Méthode de mise à jour des prédictions (non implémentée).
+        """
         pass
 
     def update_bounds(self):
+        """
+        Met à jour les bornes des nœuds.
+        """
         def recursive_update(node):
             if node.is_leaf:
                 return
@@ -83,9 +117,15 @@ class Decision_Tree:
         recursive_update(self.root)
 
     def pred(self, sample):
+        """
+        Prédit la classe d'un échantillon.
+        """
         return self._predict_sample(sample, self.root)
 
     def _predict_sample(self, sample, node):
+        """
+        Prédit la classe pour un échantillon donné à partir d'un nœud.
+        """
         if node.is_leaf:
             return node.value
         if sample[node.feature] <= node.threshold:
@@ -94,12 +134,18 @@ class Decision_Tree:
             return self._predict_sample(sample, node.right_child)
 
     def predict(self, X):
+        """
+        Prédit les classes pour un ensemble d'échantillons.
+        """
         predictions = [
             self._predict_sample(sample, self.root) for sample in X
         ]
         return np.array(predictions)
 
     def get_leaves(self):
+        """
+        Récupère les feuilles de l'arbre.
+        """
         leaves = []
 
         def recursive_collect_leaves(node):
