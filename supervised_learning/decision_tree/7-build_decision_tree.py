1b5ef16d9fd6faed4390597615d3b19ac19eddb1
Giang-Davy <davy.dangvu@gmail.com>
2024-12-19 18:02:35 +0000

    7V5

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -1,42 +1,51 @@
 #!/usr/bin/env python3
-"""fonction"""
-
 
 import numpy as np
+from sklearn import datasets
+
+# Fonction pour générer un ensemble de données
+def circle_of_clouds(n_clouds, n_objects_by_cloud, radius=1, sigma=None, seed=0, angle=0):
+    rng = np.random.default_rng(seed)
+    if not sigma:
+        sigma = np.sqrt(2 - 2 * np.cos(2 * np.pi / n_clouds)) / 7
+
+    def rotate(x, k):
+        theta = 2 * k * np.pi / n_clouds + angle
+        m = np.matrix([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+        return np.matmul(x, m)
+
+    def cloud():
+        return (rng.normal(size=2 * n_objects_by_cloud) * sigma).reshape(n_objects_by_cloud, 2) + np.array([radius, 0])
+
+    def target():
+        return np.array(([[i] * n_objects_by_cloud for i in range(n_clouds)]), dtype="int32").ravel()
+
+    return np.concatenate([np.array(rotate(cloud(), k)) for k in range(n_clouds)], axis=0), target()
 
 
-def fit(self, explanatory, target, verbose=0):
-    """
-    Args: ff
-    Returns: ff
-    """
-    pass
+def iris():
+    iris = datasets.load_iris()
+    return iris.data, iris.target
 
 
-print("-" * 52)
-print("circle of clouds :")
-print("  Training finished.")
-print("    - Depth                     : 10")
-print("    - Number of nodes           : 81")
-print("    - Number of leaves          : 41")
-print("    - Accuracy on training data : 1.0")
-print("    - Accuracy on test          : 0.9666666666666667")
-print("-" * 52)
+def wine():
+    wine = datasets.load_wine()
+    return wine.data, wine.target
 
-print("iris dataset :")
-print("  Training finished.")
-print("    - Depth                     : 10")
-print("    - Number of nodes           : 29")
-print("    - Number of leaves          : 15")
-print("    - Accuracy on training data : 0.9629629629629629")
-print("    - Accuracy on test          : 1.0")
-print("-" * 52)
 
-print("wine dataset :")
-print("  Training finished.")
-print("    - Depth                     : 10")
-print("    - Number of nodes           : 59")
-print("    - Number of leaves          : 30")
-print("    - Accuracy on training data : 0.906832298136646")
-print("    - Accuracy on test          : 0.8235294117647058")
-print("-" * 52)
+def split(explanatory, target, seed=0, proportion=.1):
+    rng = np.random.default_rng(seed)
+    test_indices = rng.choice(target.size, int(target.size * proportion), replace=False)
+    test_filter = np.zeros_like(target, dtype="bool")
+    test_filter[test_indices] = True
+
+    return {"train_explanatory": explanatory[np.logical_not(test_filter), :],
+            "train_target": target[np.logical_not(test_filter)],
+            "test_explanatory": explanatory[test_filter, :],
+            "test_target": target[test_filter]}
+
+
+# Main
+for d, name in zip([split(*circle_of_clouds(10, 30)), split(*iris()), split(*wine())],
+                   ["circle of clouds", "iris dataset", "wine dataset"]):
+    print("OK")
