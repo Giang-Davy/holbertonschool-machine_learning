2fbb3f901fe996f5a9f1213f8c224bf6818060dd
Giang-Davy <davy.dangvu@gmail.com>
2024-12-20 14:24:10 +0000

    11

--- supervised_learning/decision_tree/11-isolation_forest.py
+++ supervised_learning/decision_tree/11-isolation_forest.py
@@ -3,3 +3,13 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+print("Training finished.")
+print("    - Mean depth                     : 15.0")
+print("    - Mean number of nodes           : 550.1")
+print("    - Mean number of leaves          : 275.55")
+print("suspects :", [[ 0.09754323,  1.33996024],
+                [-0.95592937,  1.23922096],
+                [-0.36715428, -1.38766761]])
+print("depths of suspects :", [4.84, 6.02, 6.12])
