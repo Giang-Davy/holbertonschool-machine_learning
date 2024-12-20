7c40071be6efa6b565587d4dc5a6d03ecb20ecad
Giang-Davy <davy.dangvu@gmail.com>
2024-12-20 13:42:46 +0000

    8V5

--- supervised_learning/decision_tree/8-build_decision_tree.py
+++ supervised_learning/decision_tree/8-build_decision_tree.py
@@ -5,41 +5,58 @@ import numpy as np
 from sklearn import datasets
 
 
-class Decision_Tree:
-    def __init__(self, split_criterion="Gini", max_depth=3, seed=0):
-        self.split_criterion = split_criterion
-        self.max_depth = max_depth
-        self.seed = seed
-        # initialise les autres attributs ou méthodes nécessaires
-        pass
-
-    def Gini_split_criterion_one_feature(self, node, feature):
-        pass
-
-    def possible_thresholds(self, node, feature):
-        pass
-
-    def Gini_split_criterion(self, node):
-        pass
+def circle_of_clouds(n_clouds, n_objects_by_cloud, radius=1, sigma=None, seed=0, angle=0):
+    """
+    This function returns a dataset made of 'n_clouds' classes.
+    Each class is a small gaussian cloud containing 'n_objects_by_cloud' points.
+    The centers of the clouds are regularly disposed on a circle of radius 'radius' (and center (0,0)).
+    The spreadth of the clouds is governed by 'sigma'.
+    """
+    rng = np.random.default_rng(seed)
+    if not sigma:
+        sigma = np.sqrt(2 - 2 * np.cos(2 * np.pi / n_clouds)) / 7
 
     def rotate(x, k):
+        theta = 2 * k * np.pi / n_clouds + angle
+        m = np.matrix([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
         pass
 
     def cloud():
-        pass
+        pass  # (rng.normal(size=2 * n_objects_by_cloud) * sigma).reshape(n_objects_by_cloud, 2) + np.array([radius, 0])
 
     def target():
-        pass
+        pass  # np.array(([[i] * n_objects_by_cloud for i in range(n_clouds)]), dtype="int32").ravel()
 
-    def iris():
-        pass
+    pass  # np.concatenate([np.array(rotate(cloud(), k)) for k in range(n_clouds)], axis=0), target()
 
-    def wine():
-        pass
 
-    def split(explanatory, target, seed=0, proportion=.1):
-        pass
+def iris():
+    """ Returns the explanatory features and the target of the famous iris dataset """
+    iris = datasets.load_iris()
+    pass  # iris.data, iris.target
 
+
+def wine():
+    """ Returns the explanatory features and the target of the wine dataset """
+    wine = datasets.load_wine()
+    pass  # wine.data, wine.target
+
+
+#                                     #########################
+#                                     #    Data preparation   #
+#                                     #########################
+
+def split(explanatory, target, seed=0, proportion=.1):
+    """ Returns a dictionary containing a a training dataset and a test dataset """
+    rng = np.random.default_rng(seed)
+    test_indices = rng.choice(target.size, int(target.size * proportion), replace=False)
+    test_filter = np.zeros_like(target, dtype="bool")
+    test_filter[test_indices] = True
+
+    pass  # {"train_explanatory": explanatory[np.logical_not(test_filter), :],
+            # "train_target": target[np.logical_not(test_filter)],
+            # "test_explanatory": explanatory[test_filter, :],
+            # "test_target": target[test_filter]}
 
 #NE PAS TOUCHER
 print("-" * 52)
