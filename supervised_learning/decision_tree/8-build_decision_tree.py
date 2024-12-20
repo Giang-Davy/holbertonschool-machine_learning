56efe15ce9de8c78f64e7ba8ae5bd6df4b246f11
Giang-Davy <davy.dangvu@gmail.com>
2024-12-20 13:24:33 +0000

    8V5

--- supervised_learning/decision_tree/8-build_decision_tree.py
+++ supervised_learning/decision_tree/8-build_decision_tree.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python3
 
 
-import numpy as np
-from sklearn import datasets
-
-# Simulation des classes et fonctions nécessaires pour afficher les résultats
-class Decision_Tree:
-    def __init__(self, split_criterion="Gini", max_depth=20, seed=0):
-        self.split_criterion = split_criterion
-        self.max_depth = max_depth
-        self.seed = seed
-        self.tree = None
-        self.trained = False
-
-    def fit(self, X, y, verbose=0):
-        """Simule un entraînement de modèle"""
-        self.trained = True
-
-    def update_predict(self):
-        pass
-
-    def accuracy(self, X, y):
-        """Retourne une précision simulée pour les tests"""
-        return 1.0
-
 #NE PAS TOUCHER
 print("-" * 52)
 # Main
