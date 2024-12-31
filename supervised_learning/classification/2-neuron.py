d5011b1b80581a6d6318f2df37b2ea795497bfa6
Giang-Davy <davy.dangvu@gmail.com>
2024-12-31 11:41:44 +0000

    2

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""fonction"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    Classe qui présente les neuronnes
+    """
+    def __init__(self, nx):
+        """
+        Args: ff
+        Returns: ff
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive")
+        self._W = np.random.randn(1, nx)
+        self._b = 0
+        self._A = 0
+
+    @property
+    def W(self):
+        """getter function"""
+        return self._W
+
+    @property
+    def b(self):
+        """getter function"""
+        return self._b
+
+    @property
+    def A(self):
+        """getter function"""
+        return self._A
+    
+    def forward_prop(self, X):
+        """ffland"""
+        Z = np.dot(self._W, X) + self._b
+        self._A = 1 / (1 + np.exp(-Z))
+        return self._A
