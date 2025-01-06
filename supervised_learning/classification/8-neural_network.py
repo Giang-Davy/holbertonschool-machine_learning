4c62b651ccc7d9940b5a5d9ae04269375af8ba58
Giang-Davy <davy.dangvu@gmail.com>
2025-01-06 13:15:19 +0000

    8 et 7

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -115,3 +115,22 @@ class Neuron:
         # Retourne les prédictions et le coût final
         predictions, cost = self.evaluate(X, Y)
         return predictions, cost
+
+class NeuralNetwork:
+    """Réseau neuronne"""
+    def __init__(self, nx, nodes):
+        """ff"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
