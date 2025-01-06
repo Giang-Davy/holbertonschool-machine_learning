6cbcc9b4f924c73c73784b196bb4e26a4ae60bf0
Giang-Davy <davy.dangvu@gmail.com>
2025-01-06 17:13:10 +0000

    13V4

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -87,16 +87,14 @@ class NeuralNetwork:
         dZ2 = A2 - Y
         dW2 = np.dot(dZ2, A1.T) / m
         db2 = np.sum(dZ2, axis=1, keepdims=True) / m
-
-        # Mise à jour des paramètres de la deuxième couche
-        self.__W2 -= alpha * dW2
-        self.__b2 -= alpha * db2
-
         # Calcul des dérivées pour la première couche
         dZ1 = np.dot(self.__W2.T, dZ2) * (A1 * (1 - A1))
         dW1 = np.dot(dZ1, X.T) / m
         db1 = np.sum(dZ1, axis=1, keepdims=True) / m
 
+        # Mise à jour des paramètres de la deuxième couche
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
         # Mise à jour des paramètres de la première couche
         self.__W1 -= alpha * dW1
         self.__b1 -= alpha * db1
