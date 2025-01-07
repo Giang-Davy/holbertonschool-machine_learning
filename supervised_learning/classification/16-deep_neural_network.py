6c77cb4cf55360b9aff7cf8905680efa811cca8e
Giang-Davy <davy.dangvu@gmail.com>
2025-01-07 11:43:09 +0000

    16V4

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
-    """réseau neurronne profond"""
+    """réseau neuronne profond"""
     def __init__(self, nx, layers):
         """initialisation"""
         if not isinstance(nx, int):
@@ -15,16 +15,19 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if any(not isinstance(layer, int) or layer <= 0 for layer in layers):
-            raise TypeError("layers must be a list of positive integers")
+
+        # Vérification de layers sans boucle explicite ou implicite
+        for layer in layers:
+            if not isinstance(layer, int) or layer <= 0:
+                raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
         for i in range(self.L):
-            layer_in = nx if i == 0 else layers[i - 1]
+            prev = nx if i == 0 else layers[i - 1]
             self.weights[f"W{i + 1}"] = (
-                np.random.randn(layers[i], layer_in) * np.sqrt(2 / layer_in)
+                np.random.randn(layers[i], prev) * np.sqrt(2 / prev)
             )
             self.weights[f"b{i + 1}"] = np.zeros((layers[i], 1))
