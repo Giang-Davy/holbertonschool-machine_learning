3cf2ac6acc5d40d98f3b9e180da08c133f0e1de6
Giang-Davy <davy.dangvu@gmail.com>
2025-01-07 11:38:29 +0000

    16V3

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -6,23 +6,25 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
+    """réseau neurronne profond"""
     def __init__(self, nx, layers):
+        """initialisation"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        if any(not isinstance(layer, int) or layer <= 0 for layer in layers):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for layer_idx in range(self.L):
-            layer_input = nx if layer_idx == 0 else layers[layer_idx - 1]
-            self.weights[f"W{layer_idx + 1}"] = (
-                np.random.randn(layers[layer_idx], layer_input) * np.sqrt(2 / layer_input)
+        for i in range(self.L):
+            layer_in = nx if i == 0 else layers[i - 1]
+            self.weights[f"W{i + 1}"] = (
+                np.random.randn(layers[i], layer_in) * np.sqrt(2 / layer_in)
             )
-            self.weights[f"b{layer_idx + 1}"] = np.zeros((layers[layer_idx], 1))
+            self.weights[f"b{i + 1}"] = np.zeros((layers[i], 1))
