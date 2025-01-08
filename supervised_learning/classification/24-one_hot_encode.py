67a7b0ad98555e2f3bead4044c6689462c8f260b
Giang-Davy <davy.dangvu@gmail.com>
2025-01-08 15:52:27 +0000

    24

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -6,19 +6,12 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    """ff"""
-    m = Y.shape[0]  # Nombre d'exemples
-    if m == 0:
+    if not isinstance(Y, np.ndarray) or Y.ndim != 1 or not isinstance(classes, int) or classes <= 0:
         return None
-    if classes <= 0:
-        return None
-
-    one_hot_matrix = np.zeros((classes, m))
-
-    # Remplissage de la matrice one-hot
+    m = Y.shape[0]
+    one_hot = np.zeros((classes, m))
     for i in range(m):
         if Y[i] >= classes or Y[i] < 0:
             return None
-        one_hot_matrix[Y[i], i] = 1
-
-    return one_hot_matrix
+        one_hot[Y[i], i] = 1
+    return one_hot
