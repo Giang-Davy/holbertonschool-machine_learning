ed0beb1820f17b3d5993cb54f6fda17255635a4c
Giang-Davy <davy.dangvu@gmail.com>
2025-01-13 16:01:39 +0000

    1V3

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -3,9 +3,11 @@
 
 
 import tensorflow.compat.v1 as tf
-tf.disable_eager_execution()
+# tf.disable_eager_execution()
+
 
 def create_layer(prev, n, activation):
-	initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-	layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=initializer, name='layer')
-	return layer(prev)
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=initializer, name='layer')
+    return layer(prev)
