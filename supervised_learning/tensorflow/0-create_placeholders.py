5288f1fde0fc606a5596236ba6be0fd0f101b376
Giang-Davy <davy.dangvu@gmail.com>
2025-01-13 14:39:17 +0000

    0V2

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -6,6 +6,7 @@ import tensorflow.compat.v1 as tf
 
 
 def create_placeholders(nx, classes):
+    """afficher y et y """
     x = tf.placeholder(tf.float32, shape=[None, nx], name="x")
     y = tf.placeholder(tf.float32, shape=[None, classes], name="y")
     return x, y
