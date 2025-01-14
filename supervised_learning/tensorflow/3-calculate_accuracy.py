2a7d6337dddad480f017064210419c3f4c6d94f2
Giang-Davy <davy.dangvu@gmail.com>
2025-01-14 14:08:06 +0000

    3V2

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -6,7 +6,7 @@ import tensorflow.compat.v1 as tf
 
 
 def calculate_accuracy(y, y_pred):
-    """fonction"""
-    correct_predictions = tf.equal(y, y_pred)
+    """calcule"""
+    correct_predictions = tf.equal(tf.argmax(y_pred, axis=1), tf.argmax(y, axis=1))
     accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
     return accuracy
