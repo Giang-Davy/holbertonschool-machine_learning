bcaeb60c258772e821b0cc3c442459588b9b9239
Giang-Davy <davy.dangvu@gmail.com>
2025-01-14 14:04:37 +0000

    3

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""fonction"""
+
+
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """fonction"""
+    correct_predictions = tf.equal(y, y_pred)
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    return accuracy
