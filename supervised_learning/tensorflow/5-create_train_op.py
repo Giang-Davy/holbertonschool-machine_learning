e9310cee986c7c908ff589994da48cf3b3421d82
Giang-Davy <davy.dangvu@gmail.com>
2025-01-14 15:20:07 +0000

    5

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -6,6 +6,7 @@ import tensorflow.compat.v1 as tf
 
 
 def create_train_op(loss, alpha):
+    """entrainement"""
     optimizer = tf.compat.v1.train.GradientDescentOptimizer(
                 learning_rate=alpha)
     train_op = optimizer.minimize(loss)
