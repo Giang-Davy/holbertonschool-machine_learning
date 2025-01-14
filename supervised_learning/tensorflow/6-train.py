9bf17b9a171f1f813d6c71f34050b9a5ee63e3f4
Giang-Davy <davy.dangvu@gmail.com>
2025-01-14 16:19:42 +0000

    6V4

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -4,11 +4,11 @@
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
-from calculate_accuracy import calculate_accuracy
-from calculate_loss import calculate_loss
-from create_placeholders import create_placeholders
-from create_train_op import create_train_op
-from forward_prop import forward_prop
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """
