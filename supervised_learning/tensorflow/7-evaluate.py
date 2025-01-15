72e18fcfc2637d26fabe5f588f84f4c2cb0b1925
Giang-Davy <davy.dangvu@gmail.com>
2025-01-15 15:56:31 +0000

    7V2

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -6,18 +6,20 @@ tf.disable_v2_behavior()
 
 
 def evaluate(X, Y, save_path):
-    # Charger le modèle depuis le chemin donné
-    model = tf.keras.models.load_model(save_path)
-
-    # Créer un session pour évaluer le modèle
+    # Charger le modèle sauvegardé
+    saver = tf.train.import_meta_graph(save_path + '.meta')
+    
     with tf.Session() as sess:
-        # Restaurer le modèle dans la session
-        model._make_predict_function()
+        # Restaurer les poids du modèle
+        saver.restore(sess, save_path)
         
-        # Évaluer la précision et la perte
-        loss, accuracy = model.evaluate(X, Y)
-
-        # Prédire les sorties
-        predictions = model.predict(X)
-
+        # Récupérer les tenseurs de prédiction, de perte et d'exactitude depuis le graphe
+        graph = tf.get_default_graph()
+        prediction_op = graph.get_tensor_by_name("prediction:0")  # Nom à adapter selon votre modèle
+        loss_op = graph.get_tensor_by_name("loss:0")  # Nom à adapter selon votre modèle
+        accuracy_op = graph.get_tensor_by_name("accuracy:0")  # Nom à adapter selon votre modèle
+        
+        # Calculer la prédiction, la perte et l'exactitude
+        predictions, loss, accuracy = sess.run([prediction_op, loss_op, accuracy_op], feed_dict={X: X, Y: Y})
+    
     return predictions, accuracy, loss
