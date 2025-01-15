f242d43e77cead0e5ddc22aff6ec2766fb78b0f0
Giang-Davy <davy.dangvu@gmail.com>
2025-01-15 14:58:39 +0000

    6V6

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """fonction"""
 
+
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
+
 
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
@@ -10,7 +12,9 @@ create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
+          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """
     Builds, trains, and saves a neural network classifier.
 
