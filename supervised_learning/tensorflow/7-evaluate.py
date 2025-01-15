09b24b508bcee4b260b9474baf810e5aeac0a0bc
Giang-Davy <davy.dangvu@gmail.com>
2025-01-15 16:05:40 +0000

    7V3

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -5,37 +5,37 @@ Module pour évaluer la sortie d'un réseau neuronal
 import tensorflow.compat.v1 as tf
 
 
-def evaluate(X, Y, save_path):
+def evaluate(input_data, one_hot_labels, model_path):
     """
-    alue la sortie d'un réseau neuronal
+    Évalue la sortie d'un réseau neuronal
 
     Args:
-        X: numpy.ndarray contenant les données d'entrée à évaluer
-        Y: numpy.ndarray contenant les étiquettes one-hot
-        save_path: emplacement du modèle à charger
+        input_data: numpy.ndarray contenant les données d'entrée à évaluer
+        one_hot_labels: numpy.ndarray contenant les étiquettes one-hot
+        model_path: emplacement du modèle à charger
 
     Returns:
-        prédiction du réseau, précision et perte respectivement
+        Prédiction du réseau, précision et perte respectivement
     """
     # Création d'une nouvelle session
     with tf.Session() as sess:
         # Importation du métagraphe sauvegardé
-        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver = tf.train.import_meta_graph(model_path + '.meta')
         # Restauration des variables du modèle
-        saver.restore(sess, save_path)
+        saver.restore(sess, model_path)
 
         # Récupération des tenseurs nécessaires
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
+        input_tensor = tf.get_collection('x')[0]
+        label_tensor = tf.get_collection('y')[0]
+        prediction_tensor = tf.get_collection('y_pred')[0]
+        accuracy_tensor = tf.get_collection('accuracy')[0]
+        loss_tensor = tf.get_collection('loss')[0]
 
         # Évaluation du modèle
-        feed_dict = {x: X, y: Y}
-        prediction, model_accuracy, model_cost = sess.run(
-            [y_pred, accuracy, loss],
+        feed_dict = {input_tensor: input_data, label_tensor: one_hot_labels}
+        predictions, accuracy, loss = sess.run(
+            [prediction_tensor, accuracy_tensor, loss_tensor],
             feed_dict=feed_dict
         )
 
-        return prediction, model_accuracy, model_cost
+        return predictions, accuracy, loss
