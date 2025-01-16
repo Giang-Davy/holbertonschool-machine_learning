186a9a408c679fd994e7bbb2358ce85e305e1173
Giang-Davy <davy.dangvu@gmail.com>
2025-01-16 20:49:16 +0000

    4V2

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -5,7 +5,14 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
     """entrainement"""
-    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose, shuffle=shuffle)
+    history = network.fit(
+            data,
+            labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            shuffle=shuffle)
     return history
