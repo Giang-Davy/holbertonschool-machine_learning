eb03ce8294171569ad323952769578ddbbe38d0a
Giang-Davy <davy.dangvu@gmail.com>
2025-01-16 19:19:32 +0000

    3V2

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """fonction"""
 
-import numpy as np
+
 import tensorflow.keras as K
 
 
@@ -9,7 +9,7 @@ def one_hot(labels, classes=None):
     """1-chaud"""
     if classes is None:
         classes = len(set(labels))
-    one_hot_matrix = np.zeros((len(labels), classes))
+    one_hot_matrix = K.backend.zeros((len(labels), classes))
     for i in range(len(labels)):
         one_hot_matrix[i, labels[i]] = 1
     return one_hot_matrix
