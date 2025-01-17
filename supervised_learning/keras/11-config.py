bce7ce4d5353fa8792eef4a44ae0703e5423473c
Giang-Davy <davy.dangvu@gmail.com>
2025-01-17 11:53:13 +0000

    11V2

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -6,10 +6,16 @@ import tensorflow.keras as K
 
 
 def save_config(network, filename):
-    """sauvegarde JSON"""
-    network.to_json(filename)
+    """Sauvegarde la configuration du modèle en JSON"""
+    model_json = network.to_json()
+    with open(filename, 'w') as json_file:
+        json_file.write(model_json)
+    return None
 
 
 def load_config(filename):
-    """charge JSON"""
-    return keras.models.model_from_json(filename)
+    """Charge la configuration du modèle à partir d'un fichier JSON"""
+    with open(filename, 'r') as json_file:
+        model_json = json_file.read()
+    model = K.models.model_from_json(model_json)
+    return model
