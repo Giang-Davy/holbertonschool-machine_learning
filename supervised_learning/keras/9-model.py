25946b30dc9ec24340e4572d21174007600a54b2
Giang-Davy <davy.dangvu@gmail.com>
2025-01-17 11:10:04 +0000

    9V4

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -7,19 +7,10 @@ import tensorflow.keras as K
 
 def save_model(network, filename):
     """sauvegarde modele"""
-    K.models.save_model(
-        network,
-        filepath=filename,
-        overwrite=True,
-        include_optimizer=True,
-        save_format=None
-    )
+    network.save(filname)
     return None
+
 
 def load_model(filename):
     """charge un modele"""
-    return K.models.load_model(
-        filepath=filename,
-        custom_objects=None,
-        compile=True
-    )
+    return K.models.load_model(filename)
