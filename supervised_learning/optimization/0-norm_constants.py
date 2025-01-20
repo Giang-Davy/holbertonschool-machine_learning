eb4d9163b50d927dee4d011652fdb37c701a4c93
Giang-Davy <davy.dangvu@gmail.com>
2025-01-20 14:13:55 +0000

    0 et readme

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+"""fonction"""
 
 import numpy as np
 
 
 def normalization_constants(X):
+    """calcule de la moyenne et de l'ecart type"""
     m = X.shape[0]
     mean = np.mean(X, axis=0)
     std = np.std(X, axis=0)
