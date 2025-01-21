c50e59fd06437355f2e171f02c808d06735bd719
Giang-Davy <davy.dangvu@gmail.com>
2025-01-21 16:29:56 +0000

    4V1

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -9,13 +9,13 @@ def moving_average(data, beta):
     """
     Calcule la moyenne mobile exponentielle avec correction du biais
     """
-    v_prev = 0  # Initial� 0 (ou à la première valeur de la série)
+    v_prev = 0  # Initialiser la valeur précédente à 0 (ou à la première valeur de la série)
     moving_averages = []  # Liste pour stocker les moyennes mobiles
-
-    for t, x_t in enumerate(data, 1):  # Commence à rection du biais
-        v_t = beta * v_prev + (1 - beta) * x_t  # Calcmobile
+    
+    for t, x_t in enumerate(data, 1):  # Commence à 1 pour faciliter la correction du biais
+        v_t = beta * v_prev + (1 - beta) * x_t  # Calcul de la moyenne mobile
         v_t_corrected = v_t / (1 - beta**t)  # Correction du biais
-        moving_averages.append(v_t_corrected)  # Ajoutrrigée à la liste
-        v_prev = v_t  # Mettre à jour la valeur pré itération
+        moving_averages.append(v_t_corrected)  # Ajouter la moyenne mobile corrigée à la liste
+        v_prev = v_t  # Mettre à jour la valeur précédente pour la prochaine itération
 
     return moving_averages
