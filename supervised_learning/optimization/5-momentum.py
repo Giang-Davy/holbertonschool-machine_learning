39e99f6476cde935c0d151d99d8b9dde62b4db33
Giang-Davy <davy.dangvu@gmail.com>
2025-01-21 16:48:59 +0000

    5V2

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -6,6 +6,7 @@ import numpy as np
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
+    """momentum"""
     v = beta1 * v + (1 - beta1) * grad
     var = var - alpha * v
     return var, v
