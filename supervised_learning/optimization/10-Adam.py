121256e027cda3a817eb043afc4eeb02aa4c7e00
Giang-Davy <davy.dangvu@gmail.com>
2025-01-22 16:09:33 +0000

    10

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 
 
 def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """adam ameliore"""
     optimize = tf.keras.optimizers.Adam(
             learning_rate=alpha, epsilon=epsilon, beta_1=beta1, beta_2=beta2)
     return optimize
