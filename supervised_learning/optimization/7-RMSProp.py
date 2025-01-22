26fb2fefe833ce9821b8f48760d2860330a80d93
Giang-Davy <davy.dangvu@gmail.com>
2025-01-22 11:47:57 +0000

    7

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -8,5 +8,5 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """mettre à jour les variables"""
     s = beta2 * s + (1 - beta2) * (grad ** 2)
     var = var - alpha * grad / (np.sqrt(s) + epsilon)
-    
+
     return var, s
