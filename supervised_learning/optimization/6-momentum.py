9de1612a992dc25ee5178e743f343d0bf9a9e4a2
Giang-Davy <davy.dangvu@gmail.com>
2025-01-22 11:28:20 +0000

    6V3

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -7,6 +7,6 @@ import tensorflow as tf
 
 def create_momentum_op(alpha, beta1):
     """momentum ameliore"""
-    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1)
+    optimizer = tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
 
     return optimizer
