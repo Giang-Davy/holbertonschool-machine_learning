8bf688b6c9b3e61e51712dc0ecb02a3ce9443bac
Giang-Davy <davy.dangvu@gmail.com>
2025-01-23 12:25:14 +0000

    11

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -7,5 +7,6 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """apprentissage decroissant"""
-    decayed_learning_rate = alpha / (1 + decay_rate * (global_step // decay_step))
+    decayed_learning_rate = alpha / (1 + decay_rate * (
+        global_step // decay_step))
     return decayed_learning_rate
