040100cc941281393789946644c6ebaa9b34259d
Giang-Davy <davy.dangvu@gmail.com>
2025-01-29 15:21:33 +0000

    2V3

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -5,5 +5,5 @@ import tensorflow as tf
 
 def l2_reg_cost(cost, model):
     l2_loss = [tf.reduce_sum(layer.losses) for layer in model.layers]
-    total_cost = tf.add(cost, l2_loss)
-    return total_cost
+    total_cost = tf.concat([[cost], l2_loss], axis=0)
+    return total_cost[1:]  # Return the total cost for each layer excluding the first cost
