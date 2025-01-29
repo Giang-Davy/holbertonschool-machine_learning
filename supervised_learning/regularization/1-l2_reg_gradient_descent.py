49461eb7b8ea8d0bf7a71f9a75f85a9d05db9588
Giang-Davy <davy.dangvu@gmail.com>
2025-01-29 11:38:55 +0000

    1V4

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -6,23 +6,25 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """L2-GRADIENT"""
     m = Y.shape[1]
     weights_copy = weights.copy()
-    
+
     for i in range(L, 0, -1):
         A_curr = cache["A" + str(i)]
         A_prev = cache["A" + str(i - 1)] if i > 1 else cache["A0"]
-        
+
         if i == L:
             dZ = A_curr - Y
         else:
             dZ = dA * (1 - A_curr ** 2)
-        
-        dW = (np.dot(dZ, A_prev.T) / m) + (lambtha / m) * weights_copy["W" + str(i)]
+
+        dW = (np.dot(dZ, A_prev.T) / m) + (lambtha / m) * weights_copy[
+            "W" + str(i)]
         db = np.sum(dZ, axis=1, keepdims=True) / m
-        
+
         if i > 1:
             dA = np.dot(weights_copy["W" + str(i)].T, dZ)
-        
+
         weights["W" + str(i)] -= alpha * dW
         weights["b" + str(i)] -= alpha * db
