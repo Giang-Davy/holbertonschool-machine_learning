59797b5f3bd390d9df604a6ecf117992f8c84f3e
Giang-Davy <davy.dangvu@gmail.com>
2025-01-29 11:28:58 +0000

    1V4

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -2,24 +2,18 @@
 
 
 import numpy as np
-np.allclose(student_output, expected_output, atol=1e-8)
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-	m = Y.shape[1]
-	for i in range(L, 0, -1):
-		A = cache[f"A{i}"]
-		A_prev = cache[f"A{i-1}"] if i > 1 else cache["A0"]
-		W = weights[f"W{i}"]
-		b = weights[f"b{i}"]
-		
-		if i == L:
-			dZ = A - Y
-		else:
-			dZ = (1 - A ** 2) * np.dot(weights[f"W{i+1}"].T, dZ)
-		
-		dW = (np.dot(dZ, A_prev.T) + lambtha * W) / m
-		db = np.sum(dZ, axis=1, keepdims=True) / m
-		
-		weights[f"W{i}"] -= alpha * dW
-		weights[f"b{i}"] -= alpha * db
+    m = Y.shape[1]
+    A = cache['A' + str(L)]
+    dZ = A - Y
+    for i in range(L, 0, -1):
+        dW = (1 / m) * np.dot(dZ, cache['A' + str(i - 1)].T) + (lambtha / m) * weights['W' + str(i)]
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        
+        weights['W' + str(i)] -= alpha * dW
+        weights['b' + str(i)] -= alpha * db
+
+        if i > 1:
+            dZ = np.dot(weights['W' + str(i)].T, dZ) * (1 - np.power(cache['A' + str(i - 1)], 2))
