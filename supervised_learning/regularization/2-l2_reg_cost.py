c9100bcb81028c422cb857653204003b6b5695d7
Giang-Davy <davy.dangvu@gmail.com>
2025-01-29 16:52:12 +0000

    2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+L2 Regularization Cost.
+"""
 import tensorflow as tf
 
+
 def l2_reg_cost(cost, model):
-	l2_loss = tf.add_n([tf.nn.l2_loss(var) for var in model.trainable_variables if 'kernel' in var.name])
-	return cost + tf.broadcast_to(l2_loss, cost.shape)
+    """
+    sdqsd
+    """
+    return cost + model.losses
