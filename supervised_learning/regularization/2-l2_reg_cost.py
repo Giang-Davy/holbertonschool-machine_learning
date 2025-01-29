7a3bb29d19b029de63afac7e8bdbfc47d4d061ce
Giang-Davy <davy.dangvu@gmail.com>
2025-01-29 15:16:02 +0000

    2V2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-"""fonction"""
-
 
 import tensorflow as tf
 
-
 def l2_reg_cost(cost, model):
-    """Calcul du coût avec régularisation L2"""
-    l2_regularization = 0
-    
-    # Calculer la somme des carrés des poids pour chaque couche avec L2
-    for layer in model.layers:
-        if hasattr(layer, 'kernel_regularizer') and layer.kernel_regularizer is not None:
-            l2_regularization += tf.reduce_sum(tf.square(layer.kernel))
-    
-    # Calculer le coût total avec la régularisation
-    total_cost = cost + l2_regularization
+    l2_loss = sum(tf.reduce_sum(layer.losses) for layer in model.layers)
+    total_cost = cost + l2_loss
     return total_cost
