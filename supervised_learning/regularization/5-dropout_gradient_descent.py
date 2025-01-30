616b38e7eaa19dec815396cf8aed174213f5d0bf
Giang-Davy <davy.dangvu@gmail.com>
2025-01-30 15:06:42 +0000

    5

---
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    m = Y.shape[1]
+    grads = {}
+    dZL = cache['A' + str(L)] - Y
+    grads['dW' + str(L)] = np.dot(dZL, cache['A' + str(L - 1)].T) / m
+    grads['db' + str(L)] = np.sum(dZL, axis=1, keepdims=True) / m
+    for i in range(L - 1, 0, -1):
+        dZ = np.dot(weights['W' + str(i + 1)].T, dZL) * (1 - np.tanh(cache['A' + str(i)])**2)
+        dZ *= cache['D' + str(i)]
+        dZ /= keep_prob
+        grads['dW' + str(i)] = np.dot(dZ, cache['A' + str(i - 1)].T) / m
+        grads['db' + str(i)] = np.sum(dZ, axis=1, keepdims=True) / m
+        dZL = dZ
+    for i in range(1, L + 1):
+        weights['W' + str(i)] -= alpha * grads['dW' + str(i)]
+        weights['b' + str(i)] -= alpha * grads['db' + str(i)]
+
+    return weights
