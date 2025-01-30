607f62e05b96ca43fe675b86e08ec252a2e8c217
Giang-Davy <davy.dangvu@gmail.com>
2025-01-30 14:30:01 +0000

    4V3

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
+"""fonction"""
+
+
 import numpy as np
 
+
 def dropout_forward_prop(X, weights, L, keep_prob):
-	caches = {"A0": X}
-	for couche_actuelle in range(1, L + 1):
-		W = weights[f"W{couche_actuelle}"]
-		b = weights[f"b{couche_actuelle}"]
-		A_prev = caches[f"A{couche_actuelle - 1}"]
-		output_before = np.dot(W, A_prev) + b
-		if couche_actuelle == L:
-			exp_output = np.exp(output_before - np.max(output_before, axis=0, keepdims=True))
-			A = exp_output / np.sum(exp_output, axis=0, keepdims=True)
-		else:
-			A = np.tanh(output_before)
-			D = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob).astype(float)
-			A *= D
-			A /= keep_prob
-		caches[f"A{couche_actuelle}"] = A
-		caches[f"D{couche_actuelle}"] = D
-	return caches
+    cache = {'A0': X}
+    for i in range(1, L + 1):
+        W = weights['W' + str(i)]
+        b = weights['b' + str(i)]
+        A_prev = cache['A' + str(i - 1)]
+        
+        Z = np.matmul(W, A_prev) + b
+        
+        if i == L:
+            # Softmax activation function for the last layer
+            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
+        else:
+            # Tanh activation function for hidden layers
+            A = np.tanh(Z)
+            
+            # Dropout mask (convert False/True to 0/1)
+            D = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob).astype(int)
+            A = A * D / keep_prob
+            cache['D' + str(i)] = D
+        
+        cache['A' + str(i)] = A
+    
+    return cache
