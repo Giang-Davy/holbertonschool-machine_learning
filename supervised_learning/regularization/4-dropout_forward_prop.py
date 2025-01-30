9be47188cdc0c61b4db3d9bb6c0d3c6d55152b0f
Giang-Davy <davy.dangvu@gmail.com>
2025-01-30 14:31:34 +0000

    4V3

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -11,21 +11,22 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         W = weights['W' + str(i)]
         b = weights['b' + str(i)]
         A_prev = cache['A' + str(i - 1)]
-        
+
         Z = np.matmul(W, A_prev) + b
-        
+
         if i == L:
             # Softmax activation function for the last layer
             A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
         else:
             # Tanh activation function for hidden layers
             A = np.tanh(Z)
-            
+
             # Dropout mask (convert False/True to 0/1)
-            D = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob).astype(int)
+            D = (np.random.rand(
+                A.shape[0], A.shape[1]) < keep_prob).astype(int)
             A = A * D / keep_prob
             cache['D' + str(i)] = D
-        
+
         cache['A' + str(i)] = A
-    
+
     return cache
