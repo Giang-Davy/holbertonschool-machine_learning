aae1bc7b5cdde0b3f48d7376912180d631c71f3b
Giang-Davy <davy.dangvu@gmail.com>
2025-01-31 11:14:17 +0000

    6V2

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 
 def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     """dropout_create_layer"""
-    couche = tf.keras.layers.Dense(n, activation=activation)(prev)
-    dropout = tf.keras.layers.Dropout(rate=1-keep_prob)
-    return dropout(couche, training=training)
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+    couche = tf.keras.layers.Dense(units=n, activation=activation, kernel_initializer=init)
+    dropout = couche(prev)
+    dropout = tf.keras.layers.Dropout(rate=1-keep_prob)(dropout, training=training)
+    return dropout
