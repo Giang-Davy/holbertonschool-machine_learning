593615d9b4e8071d3d897ce5733612115b7853ab
Giang-Davy <davy.dangvu@gmail.com>
2025-01-31 12:32:55 +0000

    7

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""fonction"""
+
+
+import numpy as np
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """stopper en avance"""
+
+    if cost > threshold + opt_cost:
+        return (True, count)
+    else:
+        count += 1
+    if count >= patience:
+        return (True, count)
+    return (False, count)
