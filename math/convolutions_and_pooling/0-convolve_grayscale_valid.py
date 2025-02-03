105a0a97cc18cd22ee2e137d6c987b2c3d8e8931
Giang-Davy <davy.dangvu@gmail.com>
2025-02-03 15:02:50 +0000

    0V2

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""fonction"""
+
 
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
+    """filtre-gris"""
     m, h, w = images.shape
     kh, kw = kernel.shape
     new_h = h - kh + 1
@@ -11,8 +14,11 @@ def convolve_grayscale_valid(images, kernel):
     conv_result = np.zeros((m, new_h, new_w))
 
     for i in range(m):
-        for y in range(new_h):
-            for x in range(new_w):
-                conv_result[i, y, x] = np.sum(images[i, y:y+kh, x:x+kw] * kernel)
+        # Appliquer la convolution sur chaque image
+        for idx in range(new_h * new_w):
+            y = idx // new_w
+            x = idx % new_w
+            conv_result[i, y, x] = np.sum(
+                images[i, y:y+kh, x:x+kw] * kernel)
 
     return conv_result
