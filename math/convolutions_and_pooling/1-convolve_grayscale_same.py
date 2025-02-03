48308aa7e205d36b1fc2182cb262a6e17bbc1807
Giang-Davy <davy.dangvu@gmail.com>
2025-02-03 16:25:39 +0000

    1V2

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -9,15 +9,17 @@ def convolve_grayscale_same(images, kernel):
     """filtre-gris"""
     m, h, w = images.shape
     kh, kw = kernel.shape
-    new_h = h - kh + 1
-    new_w = w - kw + 1
-    conv_result = np.zeros((m, new_h, new_w))
+    pad_h = (kh - 1) // 2
+    pad_w = (kw - 1) // 2
+    conv_result = np.zeros((m, h, w))
 
-    for i in range(new_h):
-        # Appliquer la convolution sur chaque image
-        for j in range(new_w):
+    # Appliquer le padding sur les images
+    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
+
+    for i in range(h):
+        for j in range(w):
             conv_result[:, i, j] = np.sum(
-                    images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
+                    padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
                     )
 
     return conv_result
