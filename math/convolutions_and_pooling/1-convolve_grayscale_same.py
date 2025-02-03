e8fa9535fe289e5796cd41b028bfdc0af61e366b
Giang-Davy <davy.dangvu@gmail.com>
2025-02-03 16:38:13 +0000

    1V4

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -6,26 +6,20 @@ import numpy as np
 
 
 def convolve_grayscale_same(images, kernel):
-    """filtre-gris"""
+    """filtre-gris-same"""
     m, h, w = images.shape
     kh, kw = kernel.shape
-
-    # Calcul du padding pour les noyaux impairs et pairs
     pad_h = (kh - 1) // 2
     pad_w = (kw - 1) // 2
+    padded_images = np.pad(images, ((0,), (pad_h,), (pad_w,)), mode='constant', constant_values=0)
+    new_h = h
+    new_w = w
+    conv_result = np.zeros((m, new_h, new_w))
 
-    # Appliquer le padding sur les images
-    padded_images = np.pad(
-        images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
-
-    # Initialisation du résultat de convolution
-    conv_result = np.zeros((m, h, w))
-
-    # Appliquer la convolution sur chaque image
-    for i in range(h):
-        for j in range(w):
+    for i in range(new_h):
+        for j in range(new_w):
             conv_result[:, i, j] = np.sum(
-                    padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
-                    )
+                padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
+            )
 
     return conv_result
