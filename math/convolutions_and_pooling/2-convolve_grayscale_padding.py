a3fdbcad44d0ac09d62fffb95910ad105a3d0ded
Giang-Davy <davy.dangvu@gmail.com>
2025-02-04 14:41:54 +0000

    2

---
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""fonction"""
+
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """padding_custom"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    pad_h, pad_w = padding
+    padded_images = np.pad(
+        images, ((0,), (pad_h,), (pad_w,)), mode='constant', constant_values=0)
+    new_h = h
+    new_w = w
+    conv_result = np.zeros((m, new_h, new_w))
+
+    for i in range(new_h):
+        for j in range(new_w):
+            conv_result[:, i, j] = np.sum(
+                padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
+            )
+
+    return conv_result
