a0f529c0a3957b2f2e28ac4a42ef4dbc1b517d40
Giang-Davy <davy.dangvu@gmail.com>
2025-02-07 13:03:23 +0000

    2V3

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python3
-"""backward pass of a convolutional layer"""
-
-
 import numpy as np
 
-
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
-    """Calculates the backward pass of a convolutional layer"""
-    m, h_new, w_new, c_new = dZ.shape
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
+    (m, h_new, w_new, c_new) = dZ.shape
+    (m, h_prev, w_prev, c_prev) = A_prev.shape
+    (kh, kw, c_prev, c_new) = W.shape
     sh, sw = stride
 
-    if padding == "same":
-        pad_h = int(((h_prev - 1) * sh + kh - h_new) // 2)
-        pad_w = int(((w_prev - 1) * sw + kw - w_new) // 2)
-        A_prev_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (
-            pad_w, pad_w), (0, 0)), mode='constant', constant_values=0)
-    elif padding == "valid":
-        pad_h = pad_w = 0
-        A_prev_pad = A_prev
+    dA_prev = np.zeros(A_prev.shape)
+    dW = np.zeros(W.shape)
+    db = np.zeros(b.shape)
 
-    dA_prev = np.zeros_like(A_prev_pad)
-    dW = np.zeros_like(W)
-    db = np.zeros_like(b)
+    if padding == "same":
+        pad_h = ((h_new - 1) * sh + kh - h_prev) // 2
+        pad_w = ((w_new - 1) * sw + kw - w_prev) // 2
+    else:
+        pad_h = pad_w = 0
+
+    A_prev_padded = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), 'constant')
+    dA_prev_padded = np.pad(dA_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), 'constant')
 
     for i in range(m):
         for h in range(h_new):
@@ -34,15 +28,14 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
-                    a_prev_slice = A_prev_pad[
-                        i, vert_start:vert_end, horiz_start:horiz_end, :]
-                    dA_prev[
-                        i, vert_start:vert_end, horiz_start:horiz_end, :] += W[
-                            :, :, :, c] * dZ[i, h, w, c]
-                    dW[:, :, :, c] += a_prev_slice * dZ[i, h, w, c]
+                    a_slice = A_prev_padded[i, vert_start:vert_end, horiz_start:horiz_end, :]
+                    dA_prev_padded[i, vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
                     db[:, :, :, c] += dZ[i, h, w, c]
 
-    if padding == "same":
-        dA_prev = dA_prev[:, pad_h:-pad_h, pad_w:-pad_w, :]
+        if padding == "same":
+            dA_prev[i, :, :, :] = dA_prev_padded[i, pad_h:-pad_h, pad_w:-pad_w, :]
+        else:
+            dA_prev[i, :, :, :] = dA_prev_padded[i, :, :, :]
 
     return dA_prev, dW, db
