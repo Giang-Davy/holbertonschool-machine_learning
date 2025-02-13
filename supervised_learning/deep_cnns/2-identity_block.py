083ef5c66657ae57d1f4731d0edba2874ef7e0be
Giang-Davy <davy.dangvu@gmail.com>
2025-02-13 14:38:41 +0000

    4V2

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -6,31 +6,32 @@ from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
-    """block_identity"""
+    """"block identité"""
     F11, F3, F12 = filters
     initializer = K.initializers.VarianceScaling(scale=2.0)
 
     convF11 = K.layers.Conv2D(
         filters=F11,
         kernel_size=(1, 1),
+        activation='relu',
         padding="same",
         kernel_initializer=initializer)(A_prev)
     batch1 = K.layers.BatchNormalization(axis=-1)(convF11)
-    activation1 = K.layers.ReLU()(batch1)
 
     convF13 = K.layers.Conv2D(
         filters=F3,
         kernel_size=(3, 3),
+        activation='relu',
         padding="same",
-        kernel_initializer=initializer)(activation1)
+        kernel_initializer=initializer)(batch1)
     batch2 = K.layers.BatchNormalization(axis=-1)(convF13)
-    activation2 = K.layers.ReLU()(batch2)
 
     convF12 = K.layers.Conv2D(
         filters=F12,
         kernel_size=(1, 1),
+        activation='relu',
         padding="same",
-        kernel_initializer=initializer)(activation2)
+        kernel_initializer=initializer)(batch2)
     batch3 = K.layers.BatchNormalization(axis=-1)(convF12)
     activation3 = K.layers.ReLU()(batch3)
 
