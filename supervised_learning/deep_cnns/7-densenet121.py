b60ae7861a5d3554bc6eab24d444e54ef7628a7c
Giang-Davy <davy.dangvu@gmail.com>
2025-02-14 18:26:59 +0000

    7

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -1,67 +1,60 @@
 #!/usr/bin/env python3
-"""fonction"""
+"""DenseNet-121"""
 
 from tensorflow import keras as K
-inception_block = __import__('0-inception_block').inception_block
+dense_block = __import__('5-dense_block').dense_block
+transition_layer = __import__('6-transition_layer').transition_layer
 
-def inception_network():
-	"""réseau inception"""
-	input = K.layers.Input(shape=(224, 224, 3))
-	initializer = K.initializers.VarianceScaling(scale=2.0)
+def densenet121(growth_rate=32, compression=1.0):
+    """Construit le modèle DenseNet-121"""
+    initializer = K.initializers.HeNormal(seed=0)
+    inputs = K.layers.Input(shape=(224, 224, 3))
 
-	norm1 = K.layers.BatchNormalization()(input)
-	activation1 = K.layers.ReLU()(norm1)
+    # Convolution initiale
+    conv1 = K.layers.Conv2D(
+        filters=64,
+        kernel_size=(7, 7),
+        strides=2,
+        padding="same",
+        kernel_initializer=initializer)(inputs)
+    batch_norm1 = K.layers.BatchNormalization(axis=3)(conv1)
+    activation1 = K.layers.Activation('relu')(batch_norm1)
 
-	conv1 = K.layers.Conv2D(
-		filters=64,
-		kernel_size=(7, 7),
-		activation=None,
-		padding="same",
-		strides=2,
-		kernel_initializer=initializer)(activation1)
+    # MaxPooling
+    pool1 = K.layers.MaxPooling2D(
+        pool_size=(3, 3),
+        strides=2,
+        padding="same")(activation1)
 
-	pool1 = K.layers.MaxPooling2D(
-		pool_size=(3, 3),
-		strides=(2, 2),
-		padding="same")(conv1)
-	norm2 = K.layers.BatchNormalization()(pool1)
-	activation2 = K.layers.ReLU()(norm2)
+    # Dense Block 1
+    dense1, nb_filters = dense_block(pool1, 64, growth_rate, 6)
 
-	conv2 = K.layers.Conv2D(
-		64, (1, 1), padding='same', activation=None)(activation2)
-	norm3 = K.layers.BatchNormalization()(conv2)
-	activation3 = K.layers.ReLU()(norm3)
+    # Transition Layer 1
+    transition1, nb_filters = transition_layer(dense1, nb_filters, compression)
 
-	conv3 = K.layers.Conv2D(
-		192, (3, 3), padding='same', activation=None)(activation3)
-	norm4 = K.layers.BatchNormalization()(conv3)
-	activation4 = K.layers.ReLU()(norm4)
+    # Dense Block 2
+    dense2, nb_filters = dense_block(transition1, nb_filters, growth_rate, 12)
 
-	poolbis = K.layers.MaxPooling2D(
-		(3, 3), strides=(2, 2), padding='same')(activation4)
+    # Transition Layer 2
+    transition2, nb_filters = transition_layer(dense2, nb_filters, compression)
 
-	incept3a = inception_block(poolbis, [64, 96, 128, 16, 32, 32])
-	incept3b = inception_block(incept3a, [128, 128, 192, 32, 96, 64])
-	pool2 = K.layers.MaxPooling2D(
-		pool_size=(3, 3),
-		strides=(2, 2),
-		padding="same")(incept3b)
+    # Dense Block 3
+    dense3, nb_filters = dense_block(transition2, nb_filters, growth_rate, 24)
 
-	incept4a = inception_block(pool2, [192, 96, 208, 16, 48, 64])
-	incept4b = inception_block(incept4a, [160, 112, 224, 24, 64, 64])
-	incept4c = inception_block(incept4b, [128, 128, 256, 24, 64, 64])
-	incept4d = inception_block(incept4c, [112, 144, 288, 32, 64, 64])
-	pool3 = K.layers.MaxPooling2D(
-		pool_size=(3, 3),
-		strides=(2, 2),
-		padding="same")(incept4d)
+    # Transition Layer 3
+    transition3, nb_filters = transition_layer(dense3, nb_filters, compression)
 
-	incept5a = inception_block(pool3, [256, 160, 320, 32, 128, 128])
-	incept5b = inception_block(incept5a, [384, 192, 384, 48, 128, 128])
+    # Dense Block 4
+    dense4, nb_filters = dense_block(transition3, nb_filters, growth_rate, 16)
 
-	avg_pool = K.layers.AveragePooling2D((7, 7), strides=(1, 1))(incept5b)
-	dropout = K.layers.Dropout(0.4)(avg_pool)
-	output_layer = K.layers.Dense(1000, activation='softmax')(dropout)
+    # Global Average Pooling
+    avg_pool = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                         strides=7,
+                                         padding='same')(dense4)
 
-	model = K.models.Model(inputs=input, outputs=output_layer)
-	return model
+    # Fully Connected Layer
+    outputs = K.layers.Dense(1000, activation="softmax")(avg_pool)
+
+    model = K.models.Model(inputs=inputs, outputs=outputs)
+
+    return model
