4c0c9905dcb15baf4df6e9386b222d9fea7651b5
Giang-Davy <davy.dangvu@gmail.com>
2025-02-14 14:18:18 +0000

    4

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -9,21 +9,22 @@ projection_block = __import__('3-projection_block').projection_block
 def resnet50():
     """Création du réseau ResNet"""
     input = K.layers.Input(shape=(224, 224, 3))
-    initializer = K.initializers.VarianceScaling(scale=2.0)
+    initializer = K.initializers.HeNormal(seed=0)
 
     # Conv1
     conv1 = K.layers.Conv2D(
         filters=64,
         kernel_size=(7, 7),
-        activation='relu',
         padding="same",
-        strides=2,
+        strides=(2,2),
         kernel_initializer=initializer)(input)
+    batch_norm1 = K.layers.BatchNormalization(axis=3)(conv1)
+    activation1 = K.layers.Activation('relu')(batch_norm1)
 
     pool1 = K.layers.MaxPooling2D(
         pool_size=(3, 3),
         strides=(2, 2),
-        padding="same")(conv1)
+        padding="same")(activation1)
 
                 # Block 1 (conv2)
         # Block 1 (conv2)
@@ -37,30 +38,29 @@ def resnet50():
     id_block2_1 = identity_block(proj_block2, [128, 128, 512])
     id_block2_2 = identity_block(id_block2_1, [128, 128, 512])
     id_block2_3 = identity_block(id_block2_2, [128, 128, 512])
-    id_block2_4 = identity_block(id_block2_3, [128, 128, 512])
 
     # Block 3 (conv4)
     # Projection Block ici pour ajuster la taille (de 512 à 1024)
-    proj_block3 = projection_block(id_block2_4, [256, 256, 1024], s=2)  # stride=2
+    proj_block3 = projection_block(id_block2_3, [256, 256, 1024], s=2)  # stride=2
     id_block3_1 = identity_block(proj_block3, [256, 256, 1024])
     id_block3_2 = identity_block(id_block3_1, [256, 256, 1024])
     id_block3_3 = identity_block(id_block3_2, [256, 256, 1024])
     id_block3_4 = identity_block(id_block3_3, [256, 256, 1024])
     id_block3_5 = identity_block(id_block3_4, [256, 256, 1024])
-    id_block3_6 = identity_block(id_block3_5, [256, 256, 1024])
+
 
     # Block 4 (conv5)
     # Projection Block ici pour ajuster la taille (de 1024 à 2048)
-    proj_block4 = projection_block(id_block3_6, [512, 512, 2048], s=2)  # stride=2
+    proj_block4 = projection_block(id_block3_5, [512, 512, 2048], s=2)  # stride=2
     id_block4_1 = identity_block(proj_block4, [512, 512, 2048])
     id_block4_2 = identity_block(id_block4_1, [512, 512, 2048])
-    id_block4_3 = identity_block(id_block4_2, [512, 512, 2048])
 
 
 
-    avg_pool = K.layers.AveragePooling2D((7, 7), strides=(1, 1))(id_block4_3)
-    dropout = K.layers.Dropout(0.4)(avg_pool)
-    output_layer = K.layers.Dense(1000, activation='softmax')(dropout)
+
+    avg_pool = K.layers.AveragePooling2D((7, 7), strides=(1, 1))(id_block4_2)
+
+    output_layer = K.layers.Dense(1000, activation='softmax', kernel_initializer=initializer)(avg_pool)
 
     model = K.models.Model(inputs=input, outputs=output_layer)
 
