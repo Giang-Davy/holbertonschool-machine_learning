5520666491bf96a6d3e5a9c705cb10f047426aab
Giang-Davy <davy.dangvu@gmail.com>
2025-02-16 17:26:20 +0000

    7test

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -1,62 +1,82 @@
 #!/usr/bin/env python3
-"""DenseNet-121"""
-
+"""Contains the function densenet121."""
 
 from tensorflow import keras as K
+
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
 
 def densenet121(growth_rate=32, compression=1.0):
-    """Construit le modèle DenseNet-121"""
-    initializer = K.initializers.HeNormal(seed=0)
-    inputs = K.layers.Input(shape=(224, 224, 3))
-    batch_norm1 = K.layers.BatchNormalization(axis=-1)(inputs)
-    activation1 = K.layers.Activation('relu')(batch_norm1)
-
-    # Convolution initiale
-    conv1 = K.layers.Conv2D(
-        filters=64,
-        kernel_size=(7, 7),
-        strides=2,
-        padding="same",
-        kernel_initializer=initializer)(activation1)
-
-    # MaxPooling
-    pool1 = K.layers.MaxPooling2D(
-        pool_size=(3, 3),
+    """Builds the DenseNet-121 architecture."""
+    input_1 = K.layers.Input(shape=(224, 224, 3))
+    batch_normalization = K.layers.BatchNormalization(axis=-1)(input_1)
+    activation = K.layers.Activation('relu')(batch_normalization)
+    conv7x7 = K.layers.Conv2D(
+        64, (7, 7),
         strides=(2, 2),
-        padding="same")(conv1)
+        padding='same',
+        kernel_initializer=K.initializers.HeNormal(seed=0)
+    )(activation)
 
-    # Dense Block 1
-    dense1, nb_filters = dense_block(pool1, 64, growth_rate, 6)
+    max_pooling2d = K.layers.MaxPooling2D(
+        (3, 3),
+        strides=(2, 2),
+        padding='same'
+    )(conv7x7)
 
-    # Transition Layer 1
-    transition1, nb_filters = transition_layer(dense1, nb_filters, compression)
+    dense_block_1, nb_filters = dense_block(
+        max_pooling2d, 64,
+        growth_rate, 6
+    )
+    transition_layer_1, nb_filters = transition_layer(
+        dense_block_1,
+        nb_filters,
+        compression
+    )
 
-    # Dense Block 2
-    dense2, nb_filters = dense_block(transition1, nb_filters, growth_rate, 12)
+    dense_block_2, nb_filters = dense_block(
+        transition_layer_1,
+        nb_filters,
+        growth_rate, 12
+    )
+    transition_layer_2,  nb_filters = transition_layer(
+        dense_block_2,
+        nb_filters,
+        compression
+    )
 
-    # Transition Layer 2
-    transition2, nb_filters = transition_layer(dense2, nb_filters, compression)
+    dense_block_3, nb_filters = dense_block(
+        transition_layer_2,
+        nb_filters,
+        growth_rate,
+        24
+    )
+    transition_layer_3, nb_filters = transition_layer(
+        dense_block_3,
+        nb_filters,
+        compression
+    )
 
-    # Dense Block 3
-    dense3, nb_filters = dense_block(transition2, nb_filters, growth_rate, 24)
+    dense_block_4, nb_filters = dense_block(
+        transition_layer_3,
+        nb_filters,
+        growth_rate,
+        16
+    )
 
-    # Transition Layer 3
-    transition3, nb_filters = transition_layer(dense3, nb_filters, compression)
+    average_pooling2d = K.layers.AveragePooling2D(
+        pool_size=(7, 7),
+        strides=(1, 1),
+        padding='valid'
+    )(dense_block_4)
 
-    # Dense Block 4
-    dense4, nb_filters = dense_block(transition3, nb_filters, growth_rate, 16)
+    dense = K.layers.Dense(
+        1000,
+        activation='softmax',
+        kernel_initializer=K.initializers.HeNormal(seed=0)
+    )(average_pooling2d)
 
-    # Global Average Pooling
-    avg_pool = K.layers.AveragePooling2D(pool_size=(7, 7),
-                                         strides=(1,1),
-                                         padding='valid')(dense4)
-
-    # Fully Connected Layer
-    outputs = K.layers.Dense(1000, activation="softmax", kernel_initializer=initializer)(avg_pool)
-
-    model = K.models.Model(inputs=inputs, outputs=outputs)
+    model = K.models.Model(inputs=input_1, outputs=dense)
 
     return model
