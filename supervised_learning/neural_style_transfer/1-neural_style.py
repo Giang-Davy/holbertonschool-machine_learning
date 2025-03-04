02e581b69f29f891125effe200f5a09f7a0f61c6
Giang-Davy <davy.dangvu@gmail.com>
2025-03-04 16:22:26 +0000

    test

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,97 +1,36 @@
 #!/usr/bin/env python3
-"""Fonction pour le transfert de style neural"""
 
-
-import tensorflow as tf
 import numpy as np
+from tensorflow import keras as K
 
 
 class NST:
-    """Neurone-Style-Transfert"""
-    style_layers = [
-        'block1_conv1',
-        'block2_conv1',
-        'block3_conv1',
-        'block4_conv1',
-        'block5_conv1'
-    ]
-    content_layer = 'block5_conv2'
+	def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+		if not isinstance(style_image, np.ndarray) or style_image.ndim != 3 or style_image.shape[2] != 3:
+			raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
+		if not isinstance(content_image, np.ndarray) or content_image.ndim != 3 or content_image.shape[2] != 3:
+			raise TypeError("content_image must be a numpy.ndarray with shape (h, w, 3)")
+		if not isinstance(alpha, (int, float)) or alpha < 0:
+			raise TypeError("alpha must be a non-negative number")
+		if not isinstance(beta, (int, float)) or beta < 0:
+			raise TypeError("beta must be a non-negative number")
 
-    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        """initialisation"""
-        if not (
-            isinstance(style_image, np.ndarray) and style_image.shape[-1] == 3
-        ):
-            raise TypeError(
-                "style_image must be a numpy.ndarray with shape (h, w, 3)"
-            )
-        if not (
-            isinstance(content_image, np.ndarray)
-            and content_image.shape[-1] == 3
-        ):
-            raise TypeError(
-                "content_image must be a numpy.ndarray with shape (h, w, 3)"
-            )
-        if not isinstance(alpha, (int, float)) or alpha < 0:
-            raise TypeError("alpha must be a non-negative number")
-        if not isinstance(beta, (int, float)) or beta < 0:
-            raise TypeError("beta must be a non-negative number")
+		self.style_image = self.preprocess_image(style_image)
+		self.content_image = self.preprocess_image(content_image)
+		self.alpha = alpha
+		self.beta = beta
+		self.model = self.load_model()
 
-        self.style_image = self.scale_image(style_image)
-        self.content_image = self.scale_image(content_image)
-        self.alpha = alpha
-        self.beta = beta
-        self.model = self.load_model()
+	def preprocess_image(self, image):
+		image = np.expand_dims(image, axis=0)
+		image = K.applications.vgg19.preprocess_input(image)
+		return image
 
-    @staticmethod
-    def scale_image(image):
-        if not isinstance(image, np.ndarray) or image.shape[-1] != 3:
-            raise TypeError(
-                "image must be a numpy.ndarray with shape (h, w, 3)"
-            )
-
-        height, width, _ = image.shape
-
-        if height > width:
-            h_new = 512
-            w_new = int((width / height) * 512)
-        else:
-            w_new = 512
-            h_new = int((height / width) * 512)
-
-        resized_image = tf.image.resize(
-            image, (h_new, w_new), method='bicubic')
-
-        # Convertir en float32 et normaliser
-        resized_image = tf.cast(resized_image, tf.float32) / 255.0
-        resized_image = tf.clip_by_value(resized_image, 0.0, 1.0)
-
-        return resized_image[tf.newaxis, ...]
-
-    def load_model(self):
-        # Keras API
-        modelVGG19 = tf.keras.applications.VGG19(
-                include_top=False,
-                weights='imagenet'
-                )
-
-        modelVGG19.trainable = False
-
-        # selected layers
-        selected_layers = self.style_layers + [self.content_layer]
-
-        outputs = [
-                modelVGG19.get_layer(name).output for name in selected_layers
-                ]
-
-        # construct model
-        model = tf.keras.Model([modelVGG19.input], outputs)
-
-        # for replace MaxPooling layer by AveragePooling layer
-        custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
-        tf.keras.models.save_model(model, 'vgg_base.h5')
-        model_avg = tf.keras.models.load_model(
-                'vgg_base.h5', custom_objects=custom_objects
-                )
-
-        self.model = model_avg
+	def load_model(self):
+		vgg = K.applications.VGG19(include_top=False, weights="imagenet")
+		vgg.trainable = False
+		style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+		content_layer = 'block5_conv2'
+		outputs = [vgg.get_layer(name).output for name in style_layers + [content_layer]]
+		model = K.Model(inputs=vgg.input, outputs=outputs)
+		return model
