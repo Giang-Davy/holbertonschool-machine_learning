4dde3f046d040f14ccb60cab123254c14d493660
Giang-Davy <davy.dangvu@gmail.com>
2025-03-04 15:52:07 +0000

    tout les fichier et 1 completer

--- supervised_learning/neural_style_transfer/neural.py
+++ supervised_learning/neural_style_transfer/neural.py
@@ -5,6 +5,6 @@ import os
 n = int(input("Entrez un nombre : "))
 
 for i in range(n + 1):
-	with open(f"{i}-neural_syle.py", "w") as f:
+	with open(f"{i}-neural_style.py", "w") as f:
 		f.write("#!/usr/bin/env python3\n")
-	os.chmod(f"{i}-neural_syle.py", 0o755)
+	os.chmod(f"{i}-neural_style.py", 0o755)
