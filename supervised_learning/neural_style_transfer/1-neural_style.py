c69c6d47d297febbec4143827b1c811e1f9dc83a
Giang-Davy <davy.dangvu@gmail.com>
2025-03-04 16:26:45 +0000

    test

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from tensorflow import keras as K
+from tensorflow as tf
 
 
 class NST:
@@ -23,14 +23,14 @@ class NST:
 
 	def preprocess_image(self, image):
 		image = np.expand_dims(image, axis=0)
-		image = K.applications.vgg19.preprocess_input(image)
+		image = tf.keras.applications.vgg19.preprocess_input(image)
 		return image
 
 	def load_model(self):
-		vgg = K.applications.VGG19(include_top=False, weights="imagenet")
+		vgg = tf.keras.applications.VGG19(include_top=False, weights="imagenet")
 		vgg.trainable = False
 		style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
 		content_layer = 'block5_conv2'
 		outputs = [vgg.get_layer(name).output for name in style_layers + [content_layer]]
-		model = K.Model(inputs=vgg.input, outputs=outputs)
+		model = tf.keras.Model(inputs=vgg.input, outputs=outputs)
 		return model
