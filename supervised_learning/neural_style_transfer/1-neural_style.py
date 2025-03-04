bfa82d8cb1fbe2f407598ea0ec71943607c6cd5c
Giang-Davy <davy.dangvu@gmail.com>
2025-03-04 17:27:44 +0000

    test

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
-"""
-Neural style transfer
-"""
+"""Fonction pour le transfert de style neural"""
 
-import numpy as np
 import tensorflow as tf
-
+import numpy as np
 
 class NST:
-    """
-    Class that performs tasks for neural style transfer
-    """
-
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
-                    'block4_conv1', 'block5_conv1']
+    """Neurone-Style-Transfert"""
+    style_layers = [
+        'block1_conv1',
+        'block2_conv1',
+        'block3_conv1',
+        'block4_conv1',
+        'block5_conv1'
+    ]
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        """
-        Class constructor neural style transfer
-        """
-
-        if (not isinstance(style_image, np.ndarray)
-                or style_image.shape[-1] != 3):
-            raise TypeError("style_image must be a numpy.ndarray"
-                            " with shape (h, w, 3)")
+        """initialisation"""
+        if not (
+            isinstance(style_image, np.ndarray) and style_image.shape[-1] == 3
+        ):
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
         else:
             self.style_image = self.scale_image(style_image)
 
-        if (not isinstance(content_image, np.ndarray)
-                or content_image.shape[-1] != 3):
-            raise TypeError("content_image must be a numpy.ndarray"
-                            " with shape (h, w, 3)")
+        if not (
+            isinstance(content_image, np.ndarray)
+            and content_image.shape[-1] == 3
+        ):
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
         else:
             self.content_image = self.scale_image(content_image)
 
@@ -50,75 +51,45 @@ class NST:
 
     @staticmethod
     def scale_image(image):
-        """
-        rescales an image such that its pixel values are between 0 and 1
-        and its largest side is 512 px
-        """
         if not isinstance(image, np.ndarray) or image.shape[-1] != 3:
-            raise (TypeError
-                   ("image must be a numpy.ndarray with shape (h, w, 3)"))
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)"
+            )
 
-        h, w, _ = image.shape
+        height, width, _ = image.shape
 
-        if w > h:
-            w_new = 512
-            h_new = int((h * 512) / w)
-        else:
+        if height > width:
             h_new = 512
-            w_new = int((w * 512) / h)
+            w_new = int((width / height) * 512)
+        else:
+            w_new = 512
+            h_new = int((height / width) * 512)
 
-        resized_image = tf.image.resize(image,
-                                        size=[h_new, w_new],
-                                        method='bicubic')
+        resized_image = tf.image.resize(
+            image, (h_new, w_new), method='bicubic')
 
-        # Normalize
-        resized_image = resized_image / 255.0
+        # Convertir en float32 et normaliser
+        resized_image = tf.cast(resized_image, tf.float32) / 255.0
+        resized_image = tf.clip_by_value(resized_image, 0.0, 1.0)
 
-        # Limit pixel values between 0 and 1
-        resized_image = tf.clip_by_value(resized_image, 0, 1)
-
-        tf_resize_image = tf.expand_dims(resized_image, 0)
-
-        return tf_resize_image
+        return resized_image[tf.newaxis, ...]
 
     def load_model(self):
-        """
-        create the model used to calculate cost
-        """
-        # Keras API
-        modelVGG19 = tf.keras.applications.VGG19(
-                include_top=False,
-                weights='imagenet'
-                )
-
+        """Creates the model used to calculate cost"""
+        # Load the VGG19 model
+        modelVGG19 = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
         modelVGG19.trainable = False
 
-        # selected layers
+        # Select the layers
         selected_layers = self.style_layers + [self.content_layer]
+        outputs = [modelVGG19.get_layer(name).output for name in selected_layers]
 
-        outputs = [
-                modelVGG19.get_layer(name).output for name in selected_layers
-                ]
-
-        # construct model
+        # Construct the model
         model = tf.keras.Model([modelVGG19.input], outputs)
 
-        # for replace MaxPooling layer by AveragePooling layer
+        # Replace MaxPooling layers with AveragePooling layers
         custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
         tf.keras.models.save_model(model, 'vgg_base.h5')
-        model_avg = tf.keras.models.load_model(
-                'vgg_base.h5', custom_objects=custom_objects
-                )
+        model_avg = tf.keras.models.load_model('vgg_base.h5', custom_objects=custom_objects)
 
         self.model = model_avg
-
-
-
-
-
-
-
-
-
-
-
