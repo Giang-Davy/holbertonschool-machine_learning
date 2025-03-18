a47a4c2bf8633cb4477b1bcca11b541b4a1904b1
Giang-Davy <davy.dangvu@gmail.com>
2025-03-18 18:13:16 +0000

    test

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,40 +1,60 @@
 #!/usr/bin/env python3
-"""fonction"""
+"""
+A function that calculates the determinant of a matrix
+"""
+
+
+def sub_matrix(matrix, i):
+    """
+    Creates a submatrix by removing the first row and the i-th column
+    """
+    if not matrix:
+        return []
+
+    matrix2 = []
+    for row in matrix[1:]:  # Skip the first row
+        matrix2.append(row[:i] + row[i + 1:])  # Remove the i-th column
+
+    return matrix2
 
 
 def determinant(matrix):
-    """determinant d'une matrice"""
-    # Vérifie si la matrice est une liste de listes
+    """
+    Calculates the determinant of a matrix.
+    """
+    # test if matrix is a list of lists
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
     for sub_list in matrix:
         if not isinstance(sub_list, list):
             raise TypeError("matrix must be a list of lists")
 
-    # Vérifie si la matrice est vide
-    if not matrix or not matrix[0]:
+    # Special case: empty matrix (0x0)
+    if len(matrix[0]) == 0:
         return 1
 
-    # Vérifie si la matrice est carrée
+    # test if matrix is square
     if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    for sub_list in matrix:
-        if len(matrix) != len(matrix[0]):
-            raise TypeError("matrix must be a list of lists")
 
-    row = len(matrix)
-    col = len(matrix[0])
-
-    if (row, col) == (1, 1):
+    # Base cases for small matrices
+    if len(matrix) == 1:
         return matrix[0][0]
+    elif len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
 
-    if (row, col) == (2, 2):
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    # Recursive case: Calculate the determinant using cofactor expansion
+    det = 0
+    for i in range(len(matrix[0])):
+        det += (
+                ((-1) ** i) * matrix[0][i] * determinant(
+                    sub_matrix(matrix, i)
+                    )
+                )
+    return det
 
-    if (row, col) == (3, 3):
-        return (matrix[0][0] * (
-            matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1])
-                - matrix[0][1] * (
-                    matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
-                + matrix[0][2] * (
-                    matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]))
+
+
+
+
+
