a2ac35e2f2ce55dc32541a70a3ed89b1c42854da
Giang-Davy <davy.dangvu@gmail.com>
2025-03-19 20:34:40 +0000

    fixer le pycodestyle

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -107,6 +107,9 @@ def definiteness(matrix):
     if matrix.shape[0] != matrix.shape[1]:
         return None
 
+    if not np.array_equal(matrix, matrix.T):
+        return None
+
     eigenvalues = np.linalg.eigvals(matrix)
 
     if np.all(eigenvalues > 0):
