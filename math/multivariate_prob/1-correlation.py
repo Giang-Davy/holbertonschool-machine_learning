19164ca6d9bc2176edc615fccb73fe25e66e22ec
Giang-Davy <davy.dangvu@gmail.com>
2025-03-26 16:54:51 +0000

    mise des verifications

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -9,7 +9,7 @@ def correlation(C):
     """correlation"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if not len(C.shape) == 2:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     corr = np.corrcoef(C)
     return corr
