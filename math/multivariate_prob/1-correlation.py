9257d7bf68f4f2ca13368ee5ed57d262d8d240b1
Giang-Davy <davy.dangvu@gmail.com>
2025-03-26 17:11:13 +0000

    mise a jour du calcul

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -11,7 +11,7 @@ def correlation(C):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-    
+
     ecart = np.sqrt(np.diag(C))
     outer = np.outer(ecart, ecart)
     correlation_matrix = C / outer
