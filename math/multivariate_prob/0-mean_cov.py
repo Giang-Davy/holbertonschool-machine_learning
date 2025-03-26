db9ad4eb5b68826c6e2d980bca978c167d0485b4
Giang-Davy <davy.dangvu@gmail.com>
2025-03-26 16:27:23 +0000

    ajout de 0 pour calculer la covariance et la mean

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""0-mean_cov.py"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """"covariance"""
+    if not isinstance(X, np.ndarray):
+        raise TypeError("X must be a 2D numpy.ndarray")
+    
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    
+    # Calculate the mean
+    mean = np.mean(X, axis=0)
+    
+    # Calculate the covariance
+    X_centered = X - mean
+    cov = np.dot(X_centered.T, X_centered) / (n - 1)
+    
+    return mean, cov
