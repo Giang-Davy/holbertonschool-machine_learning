84bc18e5c46ff2855b7535eef1fbbcc4d343540e
Giang-Davy <davy.dangvu@gmail.com>
2025-03-31 14:01:46 +0000

    ajout d une verification

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def initialize(X, k):
     """initialisation"""
-    if not isinstance(X, np.ndarray or X.ndim != 2):
+    if not isinstance(X, np.ndarray ) or X.ndim != 2:
         return None
     mini = np.min(X, axis=0)
     maxi = np.max(X, axis=0)
