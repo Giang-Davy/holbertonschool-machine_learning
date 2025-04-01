6799f28d4e64d186d97611330c295f7bbc5cff0e
Giang-Davy <davy.dangvu@gmail.com>
2025-04-01 14:36:50 +0000

    2

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -7,6 +7,10 @@ import numpy as np
 
 def variance(X, C):
     """variance-total"""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None
+    if not isinstance(C, np.ndarray) or C.ndim != 2:
+        return None, None
     dists = np.sqrt(np.sum((X - C[:, np.newaxis]) ** 2, axis=2))
     clss = np.argmin(dists, axis=0)
     variance = np.sum(np.linalg.norm(X - C[clss], axis=1) ** 2)
