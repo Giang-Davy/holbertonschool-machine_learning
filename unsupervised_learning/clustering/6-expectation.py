07d17041d90eb11e232dfccd1282c5aa6c79dd56
Giang-Davy <davy.dangvu@gmail.com>
2025-04-02 16:57:40 +0000

    6V2

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -8,19 +8,13 @@ pdf = __import__('5-pdf').pdf
 
 def expectation(X, pi, m, S):
     """Calculates the expectation step in the EM algorithm for a GMM"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None, None
-    if not isinstance(pi, np.ndarray) or pi.ndim != 1:
-        return None, None
-    if pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]:
-        return None, None
-    if not isinstance(m, np.ndarray) or m.ndim != 2:
-        return None, None
-    if m.shape[0] != pi.shape[0] or m.shape[1] != X.shape[1]:
-        return None, None
-    if not isinstance(S, np.ndarray) or S.ndim != 3 or S.shape[2] != X.shape[1]:
-        return None, None
-    if S.shape[0] != pi.shape[0] or S.shape[1] != X.shape[1]:
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(pi, np.ndarray) or pi.ndim != 1 or
+            not isinstance(m, np.ndarray) or m.ndim != 2 or
+            not isinstance(S, np.ndarray) or S.ndim != 3 or
+            X.shape[1] != m.shape[1] or m.shape[0] != S.shape[0] or
+            S.shape[1] != S.shape[2] or
+            pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]):
         return None, None
 
     k, n = pi.shape[0], X.shape[0]
