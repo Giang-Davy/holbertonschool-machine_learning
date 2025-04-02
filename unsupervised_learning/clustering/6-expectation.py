d632e4d9bd9a6a3f30c178c049e28992c05ad2ed
Giang-Davy <davy.dangvu@gmail.com>
2025-04-02 16:41:24 +0000

    6

---
+++ unsupervised_learning/clustering/6-expectation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""6-"""
+
+
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+def expectation(X, pi, m, S):
+    """attente"""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None
+    if not isinstance(pi, np.ndarray) or pi.ndim != 1:
+        return None, None
+    if not isinstance(m, np.ndarray) or m.ndim != 2 or m.shape[0] != pi.shape[0] or m.shape[1] != X.shape[1]:
+        return None, None
+    if not isinstance(S, np.ndarray) or S.ndim != 3 or S.shape[0] != pi.shape[0] or S.shape[1] != X.shape[1] or S.shape[2] != X.shape[1]:
+        return None, None
+    
+    k, n = pi.shape[0], X.shape[0]
+    g = np.zeros((k, n))
+
+    for i in range(k):  
+        g[i] = pi[i] * pdf(X, m[i], S[i])
+    g /= np.sum(g, axis=0)
+
+    l = np.sum(np.log(np.sum(g, axis=0)))
+
+    return g, l
