918f085e74c7a855c3a8134b6360ded899279f76
Giang-Davy <davy.dangvu@gmail.com>
2025-04-02 17:05:16 +0000

    6V3

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
-"""5-pdf.py"""
+"""6-expectation"""
 
 
 import numpy as np
+pdf = __import__('5-pdf').pdf
 
 
-def pdf(X, m, S):
-    """
-    calculates the probability density function of a Gaussian distribution
-    """
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None
-    if not isinstance(m, np.ndarray) or m.ndim != 1:
-        return None
-    if not isinstance(S, np.ndarray) or S.ndim != 2:
-        return None
-    if (X.shape[1] != m.shape[0] or S.shape[0] != S.shape[1]
-            or S.shape[0] != m.shape[0]):
-        return None
+def expectation(X, pi, m, S):
+    """Calculates the expectation step in the EM algorithm for a GMM"""
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(pi, np.ndarray) or pi.ndim != 1 or
+            not isinstance(m, np.ndarray) or m.ndim != 2 or
+            not isinstance(S, np.ndarray) or S.ndim != 3 or
+            X.shape[1] != m.shape[1] or m.shape[0] != S.shape[0] or
+            S.shape[1] != S.shape[2] or
+            pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]):
+        return None, None
 
-    det_cov = np.linalg.det(S)
-    if det_cov <= 0:  # Check for invalid determinant
-        return np.full(X.shape[0], 1e-300)
+    k, n = pi.shape[0], X.shape[0]
+    g = np.zeros((k, n))
 
-    inv_cov = np.linalg.inv(S)
-    X_centered = X - m
-    coef = 1 / np.sqrt((2 * np.pi) ** X.shape[1] * det_cov)
-    exponent = -0.5 * np.sum(X_centered @ inv_cov * X_centered, axis=1)
+    for i in range(k):  
+        g[i] = pi[i] * pdf(X, m[i], S[i])
 
-    P = coef * np.exp(exponent)
-    P = np.maximum(P, 1e-300)  # Avoid probabilities too close to zero
-    return P
+    total_prob = np.sum(g, axis=0)
+    g /= total_prob
+
+    total_prob = np.maximum(total_prob, 1e-300)
+    l = np.sum(np.log(total_prob))
+
+    return g, l
