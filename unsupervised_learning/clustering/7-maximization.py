ac985438c4c685008b009733ee8df92387b95720
Giang-Davy <davy.dangvu@gmail.com>
2025-04-03 13:03:25 +0000

    7

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""7-maximization.py"""
+
+
+import numpy as np
+
+
+def maximization(X, g):
+    """maximization"""
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None, None
+    if not isinstance(g, np.ndarray) or g.ndim != 2:
+        return None, None, None
+
+    k, n = g.shape
+
+    # Calcul des priorités pi
+    pi = np.sum(g, axis=1) / np.sum(g)
+
+    # Calcul des centres m
+    m = np.dot(g, X) / np.sum(g, axis=1, keepdims=True)
+
+    # Calcul des matrices de covariance S avec une boucle
+    S = np.zeros((k, X.shape[1], X.shape[1]))  # Initialisation de S
+
+    for i in range(k):
+        X_centered = X - m[i]  # Centrer X par rapport à m[i]
+        S[i] = np.dot((g[i] * X_centered.T), X_centered) / np.sum(g[i])
+
+    return pi, m, S
