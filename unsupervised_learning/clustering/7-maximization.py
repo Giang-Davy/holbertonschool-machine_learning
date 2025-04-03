9b5dc2de961c0ef5f4b76031bb7bb64e197bb95d
Giang-Davy <davy.dangvu@gmail.com>
2025-04-03 13:13:40 +0000

    7

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -10,21 +10,22 @@ def maximization(X, g):
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None
     if not isinstance(g, np.ndarray) or g.ndim != 2:
-        return None, None, None 
-    if g.shape[1] !=  X.shape[0]:
+        return None, None, None
+    if g.shape[1] != X.shape[0]:
+        return None, None, None
+    if np.allclose(g.sum(axis=0), 1.0):
         return None, None, None
 
-    n, d = X.shape
-    k, _ = g.shape
+    k, n = g.shape
 
     # Calcul des priorités pi
-    pi = np.sum(g, axis=1) / n
+    pi = np.sum(g, axis=1) / np.sum(g)
 
     # Calcul des centres m
     m = np.dot(g, X) / np.sum(g, axis=1, keepdims=True)
 
     # Calcul des matrices de covariance S avec une boucle
-    S = np.zeros((k, d, d))  # Initialisation de S
+    S = np.zeros((k, X.shape[1], X.shape[1]))  # Initialisation de S
 
     for i in range(k):
         X_centered = X - m[i]  # Centrer X par rapport à m[i]
