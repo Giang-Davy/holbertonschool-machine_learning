2f5dfb17427104970c6281220b5495a2eae65b81
Giang-Davy <davy.dangvu@gmail.com>
2025-04-08 11:02:10 +0000

    1

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -6,21 +6,27 @@ import numpy as np
 
 
 def regular(P):
-    """reguleur"""
-    if not isinstance(P, np.ndarray):
+    """Détermine si une matrice de transition est régulonnaire."""
+    if not isinstance(P, np.ndarray) or len(P.shape) != 2:
         return None
-    temp = P.copy()
+    if P.shape[0] != P.shape[1]:
+        return None
+    if not np.allclose(P.sum(axis=1), 1):
+        return None
+
     for _ in range(1, 101):
         temp = np.linalg.matrix_power(P, _)
         if np.all(temp > 0):
             break
     else:
         return None
+
+    # Calcule la distribution stationnaire
     s = np.ones((1, P.shape[0])) / P.shape[0]
     for _ in range(10000):
         s_copy = s @ P
         if np.allclose(s_copy, s, atol=1e-8):
-            return s
+            return s_copy
         s = s_copy
 
     return None
