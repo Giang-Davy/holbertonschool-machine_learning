f628e0c940b764112e8e1dba1fb38fdb95164029
Giang-Davy <davy.dangvu@gmail.com>
2025-04-24 13:16:56 +0000

    2

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -7,7 +7,7 @@ import tensorflow.keras as K
 
 def autoencoder(input_dims, filters, latent_dims):
     """
-    autoencoder
+    Builds a convolutional autoencoder.
     """
     # Encoder
     X = K.Input(shape=input_dims)
@@ -15,22 +15,25 @@ def autoencoder(input_dims, filters, latent_dims):
     for nodes in filters:
         x = K.layers.Conv2D(
             filters=nodes, kernel_size=(3, 3), padding='same', activation='relu')(x)
-        x = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+        x = K.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(x)
     encoder = K.Model(inputs=X, outputs=x)
 
     # Decoder
     latent_input = K.Input(shape=latent_dims)
     x = latent_input
     for idx, nodes in enumerate(reversed(filters)):
+        x = K.layers.Conv2D(
+            filters=nodes, kernel_size=(3, 3), padding='same', activation='relu')(x)
         x = K.layers.UpSampling2D(size=(2, 2))(x)
-        x = K.layers.Conv2D(filters=nodes, kernel_size=(3, 3), padding='same', activation='relu')(x)
     x = K.layers.Conv2D(
         filters=input_dims[2], kernel_size=(3, 3), activation='sigmoid', padding='same')(x)
 
+    # Crop the output to match the input dimensions
     x = K.layers.Cropping2D(cropping=((2, 2), (2, 2)))(x)
+
     decoder = K.Model(inputs=latent_input, outputs=x)
 
-    # Autoencoder complet
+    # Autoencoder complete
     auto_input = K.Input(shape=input_dims)
     encoded = encoder(auto_input)
     decoded = decoder(encoded)
