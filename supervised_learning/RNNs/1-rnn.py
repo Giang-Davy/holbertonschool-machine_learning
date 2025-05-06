3d16c84005ad45a04b06430150fa6771bcee511f
Giang-Davy <davy.dangvu@gmail.com>
2025-05-06 16:48:08 +0000

    1

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 """1-rnn.py"""
 
+
 import numpy as np
 
+
 def rnn(rnn_cell, X, h_0):
-    """Forward propagation"""
+    """foward propagation"""
     t, m, i = X.shape
     h_prev = h_0
-    H = [h_0]  # Inclure l'état initial h_0 dans H
+    H = [h_0]  # il faut ajouter h_0 initialement
     Y = []
     for k in range(t):
         x_t = X[k]
         h_next, y = rnn_cell.forward(h_prev, x_t)
-        H.append(h_next)  # Ajouter h_next à H
-        Y.append(y)       # Ajouter y à Y
-        h_prev = h_next   # Mettre à jour h_prev pour l'itération suivante
+        H.append(h_next)
+        Y.append(y)
+        h_prev = h_next
     return np.array(H), np.array(Y)
