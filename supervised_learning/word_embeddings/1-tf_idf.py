02b929e70a039a0fc61eb88838b39a6d26c3aec9
Giang-Davy <davy.dangvu@gmail.com>
2025-05-20 15:39:42 +0000

    1

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,41 +1,19 @@
 #!/usr/bin/env python3
-"""1-tf_idf.py"""
+"""1-tf_idf_sklearn.py"""
+
 
 import numpy as np
-import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
-    cleaned = []
-    for sentence in sentences:
-        sentence = sentence.lower()
-        words = re.findall(r'\b[a-zA-Z]{2,}\b', sentence)
-        cleaned.append(words)
-
-    if vocab is None:
-        vocab_set = set()
-        for sentence in cleaned:
-            for word in sentence:
-                vocab_set.add(word)
-        features = sorted(vocab_set)
+    if vocab is not None:
+        vectorizer = TfidfVectorizer(vocabulary=vocab)
     else:
-        features = vocab
+        vectorizer = TfidfVectorizer()
 
-    s = len(sentences)
-    f = len(features)
-    embeddings = np.zeros((s, f), dtype=float)
-    for i, sentence in enumerate(cleaned):
-        for word in sentence:
-            tf = sentence.count(word) / len(sentence)
-            if word in features:
-                index = features.index(word)
-                embeddings[i, index] = tf
-
-    idfs = np.zeros(f, dtype=float)
-    for j, word in enumerate(features):
-        d_w = sum(1 for sentence in cleaned if word in sentence)
-        idfs[j] = np.log((1 + s) / (1 + d_w)) + 1
-
-    embeddings *= idfs
+    tfidf_matrix = vectorizer.fit_transform(sentences)
+    embeddings = tfidf_matrix.toarray()
+    features = vectorizer.get_feature_names_out()
 
     return embeddings, features
