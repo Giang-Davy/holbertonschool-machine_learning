79eeb964a2c949534095c1a53741cf797ffdf2fe
Giang-Davy <davy.dangvu@gmail.com>
2025-05-21 10:29:58 +0000

    2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -8,24 +8,23 @@ import gensim
 def word2vec_model(sentences, vector_size=100, min_count=5,
                    window=5, negative=5,
                    cbow=True, epochs=5, seed=0, workers=1):
-    """"entrainement word2vec"""
-
-    if cbow == True:
-        sg = 0
-    if cbow == False:
-        sg = 1
+    """Train a Word2Vec model."""
+    sg = 0 if cbow else 1
 
     model = gensim.models.Word2Vec(
-    vector_size=vector_size,
-    min_count=min_count,
-    window=window,
-    negative=negative,
-    sg=sg,
-    seed=seed,
-    workers=workers
-)
+        sentences=sentences,  # mettre ici aussi le sentences
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        seed=seed,
+        workers=workers
+    )
     model.build_vocab(sentences)
-    model.train(sentences,                 # Entraînement manuel
-            total_examples=model.corpus_count,
-            epochs=epochs)
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=epochs
+    )
     return model
