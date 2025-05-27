d0921b759c120ff6081f6ae3cba0b80832922f08
Giang-Davy <davy.dangvu@gmail.com>
2025-05-27 15:15:33 +0000

    2

---
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""2-rnn_decoder.py"""
+
+
+import tensorflow as tf
+SelfAttention = __import__('1-self_attention').SelfAttention
+
+
+class RNNDecoder(tf.keras.layers.Layer):
+    """RNNDecoder"""
+    def __init__(self, vocab, embedding, units, batch):
+        """initialisation"""
+        super(RNNDecoder, self).__init__()
+        self.attention = SelfAttention(units)
+        self.embedding = tf.keras.layers.Embedding(
+            input_dim=vocab, output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(units=units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+        self.F = tf.keras.layers.Dense(units=vocab)
+
+    def call(self, x, s_prev, hidden_states):
+        """rappelle"""
+        context, weight = self.attention(s_prev, hidden_states)
+        embedded = self.embedding(x)
+        context = tf.expand_dims(context, axis=1)
+        concat_result = tf.concat([context, embedded], axis=-1)
+        output, s = self.gru(concat_result, initial_state=s_prev)
+        y = self.F(output)
+
+        return y, s
