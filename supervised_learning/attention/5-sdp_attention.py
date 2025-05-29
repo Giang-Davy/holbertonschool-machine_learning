a7742bb52745594cf4be244304e48f1b82c06645
Giang-Davy <davy.dangvu@gmail.com>
2025-05-29 10:14:25 +0000

    5

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -7,14 +7,15 @@ import tensorflow as tf
 
 def sdp_attention(Q, K, V, mask=None):
     """sdp attention calcule"""
-    #  obtenir dk sous float
-    dk = tf.cast(tf.shape(Q)[-1], tf.float32)
-    #  transposition de K
-    K_transposed = tf.transpose(K, perm=[0, 2, 1])
-    score = tf.matmul(Q, K_transposed) / tf.math.sqrt(dk)
-    if mask is not None:
-        score += (mask * -1e9)
-    weight = tf.nn.softmax(score)
-    output = tf.matmul(weight, V)
+    # Q, K, V: (..., seq_len_q, depth)
+    matmul_qk = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
 
-    return output, weight
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+    output = tf.matmul(attention_weights, V)
+
+    return output, attention_weights
