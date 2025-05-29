cb8a207b9335c01c1f6c4b263f71729923eac151
Giang-Davy <davy.dangvu@gmail.com>
2025-05-29 10:07:23 +0000

    6

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -1,49 +1,20 @@
 #!/usr/bin/env python3
-"""6-multihead_attention.py"""
+"""5-sdp_attention.py"""
 
 
 import tensorflow as tf
-sdp_attention = __import__('5-sdp_attention').sdp_attention
 
 
-class MultiHeadAttention(tf.keras.layers.Layer):
-    """classe"""
-    def __init__(self, dm, h):
-        """initialisation"""
-        super(MultiHeadAttention, self).__init__()
-        self.h = h
-        self.depth = dm // h
-        self.dm = dm
-        self.Wq = tf.keras.layers.Dense(units=dm)
-        self.Wk = tf.keras.layers.Dense(units=dm)
-        self.Wv = tf.keras.layers.Dense(units=dm)
-        self.linear = tf.keras.layers.Dense(units=dm)
+def sdp_attention(Q, K, V, mask=None):
+    """sdp attention calcule"""
+    #  obtenir dk sous float
+    dk = tf.cast(tf.shape(Q)[-1], tf.float32)
+    #  transposition de K
+    K_transposed = tf.transpose(K, perm=[0, 2, 1])
+    score = tf.matmul(Q, K_transposed) / tf.math.sqrt(dk)
+    if mask is not None:
+        score += (mask * -1e9)
+    weight = tf.nn.softmax(score)
+    output = tf.matmul(weight, V)
 
-    def call(self, Q, K, V, mask):
-        """rappelle"""
-        batch_size = tf.shape(Q)[0]
-        seq_len = tf.shape(Q)[1]
-
-        # Projeter Q, K, V
-        q = self.Wq(Q)
-        k = self.Wk(K)
-        v = self.Wv(V)
-
-        # Reshape et transpose pour multi-head
-        def split_heads(x):
-            x = tf.reshape(x, (batch_size, seq_len, self.h, self.depth))
-            return tf.transpose(x, perm=[0, 2, 1, 3])
-
-        q = split_heads(q)
-        k = split_heads(k)
-        v = split_heads(v)
-
-        # Attention
-        output, weights = sdp_attention(q, k, v, mask)
-
-        # Concaténer les têtes
-        output = tf.transpose(output, perm=[0, 2, 1, 3])
-        output = tf.reshape(output, (batch_size, seq_len, self.dm))
-        output = self.linear(output)
-
-        return output, weights
+    return output, weight
