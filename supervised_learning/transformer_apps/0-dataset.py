79aaac3fbd093f5c61035f67ee1d1a93e35ffec9
Giang-Davy <davy.dangvu@gmail.com>
2025-06-03 15:32:28 +0000

    0

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,35 +1,20 @@
 #!/usr/bin/env python3
-"""0-dataset.py"""
-
 
 import tensorflow_datasets as tfds
 import transformers
 
 
 class Dataset:
-    """classe de dataset"""
     def __init__(self):
-        """initialisation"""
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
+        self.data_train, self.data_valid = tfds.load(
+        'ted_hrlr_translate/pt_to_en',
+        split=['train', 'validation'],
+        as_supervised=True
+        )
+
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
-    def tokenize_dataset(self, data):
-        """crée deux tokenizers avec vocabulaire 2**13"""
-        pt_texts = []
-        en_texts = []
-
-        for pt, en in data:
-            pt_texts.append(pt.numpy().decode("utf-8"))
-            en_texts.append(en.numpy().decode("utf-8"))
-
-        tokenizer_pt = transformers.BertTokenizerFast.train_new_from_iterator(
-            pt_texts,
-            vocab_size=2**13
-        )
-        tokenizer_en = transformers.BertTokenizerFast.train_new_from_iterator(
-            en_texts,
-            vocab_size=2**13
-        )
-
-        return tokenizer_pt, tokenizer_en
+        def tokenize_dataset(self, data):
+            tokenizer_pt = transformers.BertTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased')
+            tokenizer_en = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+            return tokenizer_pt, tokenizer_en
