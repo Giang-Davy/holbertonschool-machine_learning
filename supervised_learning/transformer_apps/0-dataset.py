f23d7f37a0b376cd326bb4b27cd80aa837f58954
Giang-Davy <davy.dangvu@gmail.com>
2025-06-03 15:42:36 +0000

    0

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -7,14 +7,29 @@ import transformers
 class Dataset:
     def __init__(self):
         self.data_train, self.data_valid = tfds.load(
-        'ted_hrlr_translate/pt_to_en',
-        split=['train', 'validation'],
-        as_supervised=True
+            'ted_hrlr_translate/pt_to_en',
+            split=['train', 'validation'],
+            as_supervised=True
         )
 
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
-        def tokenize_dataset(self, data):
-            tokenizer_pt = transformers.BertTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased')
-            tokenizer_en = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
-            return tokenizer_pt, tokenizer_en
+    def tokenize_dataset(self, data):
+        pt_sentences = []
+        en_sentences = []
+
+        for pt, en in tfds.as_numpy(data):
+            pt_sentences.append(pt.decode('utf-8'))
+            en_sentences.append(en.decode('utf-8'))
+
+        pt_tokenizer = transformers.BertTokenizerFast.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased'
+        )
+        en_tokenizer = transformers.BertTokenizerFast.from_pretrained(
+            'bert-base-uncased'
+        )
+
+        pt_tokenizer.train_new_from_iterator(pt_sentences, vocab_size=2**13)
+        en_tokenizer.train_new_from_iterator(en_sentences, vocab_size=2**13)
+
+        return pt_tokenizer, en_tokenizer
