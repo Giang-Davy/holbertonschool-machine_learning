012ceb4f26a03d13f848f997cb7a31abb676d239
Giang-Davy <davy.dangvu@gmail.com>
2025-06-03 15:28:20 +0000

    0

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -12,18 +12,24 @@ class Dataset:
         """initialisation"""
         self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
-        self.tokenizer_pt = transformers.AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased')
-        self.tokenizer_en = transformers.AutoTokenizer.from_pretrained('bert-base-uncased')
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
-        """Tokenize le dataset"""
+        """crée deux tokenizers avec vocabulaire 2**13"""
         pt_texts = []
         en_texts = []
+
         for pt, en in data:
-            pt_texts.append(pt.numpy().decode('utf-8'))
-            en_texts.append(en.numpy().decode('utf-8'))
+            pt_texts.append(pt.numpy().decode("utf-8"))
+            en_texts.append(en.numpy().decode("utf-8"))
 
-        tokenized_pt = self.tokenizer_pt(pt_texts, padding=True, truncation=True, return_attention_mask=False)["input_ids"]
-        tokenized_en = self.tokenizer_en(en_texts, padding=True, truncation=True, return_attention_mask=False)["input_ids"]
+        tokenizer_pt = transformers.BertTokenizerFast.train_new_from_iterator(
+            pt_texts,
+            vocab_size=2**13
+        )
+        tokenizer_en = transformers.BertTokenizerFast.train_new_from_iterator(
+            en_texts,
+            vocab_size=2**13
+        )
 
-        return tokenized_pt, tokenized_en
+        return tokenizer_pt, tokenizer_en
