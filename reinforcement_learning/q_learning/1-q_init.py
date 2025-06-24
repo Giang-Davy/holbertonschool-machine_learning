e67f4ad7256707cf8e917094c32d31b5c7bd0698
Giang-Davy <davy.dangvu@gmail.com>
2025-06-24 11:59:25 +0000

    0

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""1-q_init.py"""
+
+
+import gymnasium as gym
+import numpy as np
+
+
+def q_init(env):
+    """cree une q table"""
+
+    q = np.zeros((env.observation_space.n, env.action_space.n))
+
+    return q
