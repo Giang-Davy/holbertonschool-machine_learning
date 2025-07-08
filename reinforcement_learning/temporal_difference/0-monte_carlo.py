2f5ab0a857aaa08fb99d568be164c9940b0d1aef
Giang-Davy <davy.dangvu@gmail.com>
2025-07-08 14:34:14 +0000

    0

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -4,28 +4,26 @@
 import numpy as np
 
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+def monte_carlo(env, V, policy, episodes=5000,
+                max_steps=100, alpha=0.1, gamma=0.99):
     """Monte Carlo Algorithm"""
 
-    for i in range (episodes):
+    for i in range(episodes):
         episode = []
-        state = env.reset()
-        if isinstance(state, tuple):
-            state = state[0]
+        state = env.reset()[0]  # régler le problème de tuple
         for step in range(max_steps):
             action = policy(state)
-            next_state, reward, terminated, truncated, info = env.step(
-                action)
-            if isinstance(state, tuple):
-                state = state[0]
-            done = terminated or truncated
+            next_state, reward, terminated, truncated, info = env.step(action)
             episode.append((state, reward))
 
-            if done:
+            if terminated or truncated:
                 break
             state = next_state
         G = 0
+        episode = np.array(episode, dtype=int)
         for state, reward in reversed(episode):
             G = reward + gamma * G
-            V[state] += alpha * (G - V[state])
+            if state not in episode[:i, 0]:
+                # Update the value function V(s)
+                V[state] = V[state] + alpha * (G - V[state])
     return V
