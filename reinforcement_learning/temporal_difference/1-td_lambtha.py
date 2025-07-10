7a67edb49c80a88e5416bf1be86ec76819d67921
Giang-Davy <davy.dangvu@gmail.com>
2025-07-10 10:18:12 +0000

    1

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -11,20 +11,24 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000,
     for i in range(episodes):
         e = np.zeros_like(V)
         state = env.reset()[0]  # régler le problème de tuple
-        
+
         for step in range(max_steps):
             action = policy(state)
             next_state, reward, terminated, truncated, info = env.step(action)
 
+            delta = reward + gamma * V[next_state] - V[state]
+
+            # Incrémente la trace d'éligibilité pour l'état courant
+            e[state] += 1
+
+            # Met à jour tous les états selon leur trace d'éligibilité
+            V += alpha * delta * e
+
+            # Décroît toutes les traces d'éligibilité
+            e *= gamma * lambtha
+
+            state = next_state
+
             if terminated or truncated:
                 break
-            delta = reward + gamma * V[next_state] - V[state]
-            
-            for s in range(len(V)):
-                if s == state:
-                    e[s] = gamma * lambtha * e[s] + 1
-                else:
-                    e[s] = gamma * lambtha * e[s]
-                V[s] = V[s] + alpha * delta * e[s]
-            state = next_state
     return V
