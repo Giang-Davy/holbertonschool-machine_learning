8782497ddda9e3cde9f887e2043d43596a7e9d36
Giang-Davy <davy.dangvu@gmail.com>
2025-07-16 15:52:18 +0000

    mise a jour de policy

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -12,3 +12,14 @@ def policy(matrix, weight):
     S = np.sum(z, axis=1, keepdims=True)
     pi = z / S
     return pi
+
+
+def policy_gradient(state, weight):
+    """politique gradiente"""
+    politique = policy(state.reshape(1, -1), weight)[0]  # 1D vector
+    action = np.random.choice(range(len(politique)), p=politique)
+    vecteur = np.zeros_like(politique)
+    vecteur[action] = 1
+    diff = vecteur - politique
+    gradient = np.outer(state, diff)
+    return action, gradient
