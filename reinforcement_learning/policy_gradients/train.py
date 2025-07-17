38bfeb7ad0938a920bcae8a04b709dc3eaf63408
Giang-Davy <davy.dangvu@gmail.com>
2025-07-17 13:43:25 +0000

    train

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -9,7 +9,7 @@ policy_gradient = __import__('policy_gradient').policy_gradient
 def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
     """entrainement"""
     score_list = []
-    weight = np.random.randn(env.observation_space.shape[0], env.action_space.n)
+    weight = np.random.randn(env.observation_space.shape[0], env.action_space.n) * 0.01
     for episode in range(nb_episodes):
         state = env.reset()
         if isinstance(state, tuple):
@@ -28,10 +28,12 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         score_list.append(score)
         print("Episode: {} Score: {}".format(episode, score))
         G = 0
+        gradients_sum = np.zeros_like(weight)
         for reward, gradient in reversed(description):
             if not isinstance(gradient, np.ndarray):
                 gradient = np.array(gradient)
             G = reward + gamma * G
-            weight += alpha * gradient * G
+            gradients_sum += gradient * G
+        weight += alpha * gradients_sum
         
     return score_list
