fa543262a0098393de387baf56d576c0dd139fcc
Giang-Davy <davy.dangvu@gmail.com>
2025-07-21 12:42:53 +0000

    update 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -7,7 +7,11 @@ import pandas as pd
 
 def from_numpy(array):
     """création de dataframe"""
-
-    data_frame = pd.DataFrame(data=array)
+    liste = []
+    colonnes = array.shape[1]
+    for i in range(colonnes):
+        lettre = chr(i+65)
+        liste.append(lettre)
+    data_frame = pd.DataFrame(data=array, columns=liste)
 
     return data_frame
