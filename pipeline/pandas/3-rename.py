55b43d201b4d2ae4a8e61c99880f59efac0f0623
Giang-Davy <davy.dangvu@gmail.com>
2025-07-22 15:56:58 +0000

    ajout du code 3

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""3-rename.py"""
+
+
+import pandas as pd
+
+
+def rename(df):
+    """changer de nom"""
+
+    df = pd.DataFrame(df[['Timestamp', 'Close']])
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
+    df = df.rename(columns={'Timestamp': 'DateTime'})
+
+    return df
