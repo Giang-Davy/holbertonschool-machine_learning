3426ac762eb7a97d7ef03b98194cf5d0a1ba9ceb
Giang-Davy <davy.dangvu@gmail.com>
2025-07-23 10:58:28 +0000

    enlevement de l import panda

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -2,10 +2,8 @@
 """4-array.py"""
 
 
-import pandas as pd
-
-
 def array(df):
-    """tableau"""
+    """prendre les 10 dernières valeurs de High et Close
+    et le convertir en np"""
     valeur_select = df[["High", "Close"]].tail(10)
     return valeur_select.values
