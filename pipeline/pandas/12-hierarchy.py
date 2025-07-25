825ad8d2fe100d7e4ec011b1640a10b200e27b24
Giang-Davy <davy.dangvu@gmail.com>
2025-07-25 12:16:54 +0000

    update du 12

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -9,11 +9,12 @@ index = __import__('10-index').index
 def hierarchy(df1, df2):
     """ranger"""
 
-    df1 = df1.loc[df1["Timestamp"].between(1417411980, 1417417980)]
-    df2 = df2.loc[df2["Timestamp"].between(1417411980, 1417417980)]
     df1 = index(df1)
     df2 = index(df2)
+    df1 = df1.loc[df1.index.to_series().between(1417411980, 1417417980)]
+    df2 = df2.loc[df2.index.to_series().between(1417411980, 1417417980)]
     df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
     df = df.swaplevel(0, 1)
+    df = df.sort_index(level=0)
 
     return df
