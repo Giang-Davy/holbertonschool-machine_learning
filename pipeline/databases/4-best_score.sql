a525df53065ceef3a1902711386c92cb68c20e2a
Giang-Davy <davy.dangvu@gmail.com>
2025-08-05 12:50:53 +0200

    ajout du code 4

--- pipeline/databases/4-best_score.sql
+++ pipeline/databases/4-best_score.sql
@@ -1,2 +1,2 @@
--- afficher seulement oÙ le score est egal ou plus de 10
-SELECT * FROM second_table WHERE score >= 10;
+-- afficher seulement oÙ le score est egal ou plus de 10 et affiche score en premiere colonne
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
