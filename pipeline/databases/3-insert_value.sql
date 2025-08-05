44ce155ef4f4430d72022187790c3c560b5e44fc
Giang-Davy <davy.dangvu@gmail.com>
2025-08-05 12:09:44 +0200

    mise a jour du code 3

--- pipeline/databases/3-insert_value.sql
+++ pipeline/databases/3-insert_value.sql
@@ -1,4 +1,2 @@
 -- insertion des valeurs aux colonnes
 INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
-INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
-INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
