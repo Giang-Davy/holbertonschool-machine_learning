67ce1a44911cc2a538c9bab0603c33ccd809e666
Giang-Davy <davy.dangvu@gmail.com>
2025-08-06 13:02:34 +0200

    ajout du code 9

---
+++ pipeline/databases/9-no_genre.sql
@@ -0,0 +1,3 @@
+-- Affiche les noms des series qui n'ont pas de genre id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv
+_shows.title ASC, tv_show_genres.genre_id ASC;
