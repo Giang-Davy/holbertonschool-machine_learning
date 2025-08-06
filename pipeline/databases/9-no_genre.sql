7202f221a7806106b41848062ff17d648698f35f
Giang-Davy <davy.dangvu@gmail.com>
2025-08-06 13:04:08 +0200

    correction synthax

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,3 +1,2 @@
 -- Affiche les noms des series qui n'ont pas de genre id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv
-_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
