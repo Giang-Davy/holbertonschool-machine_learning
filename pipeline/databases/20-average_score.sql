277eebe37147d3960c7326ce30696ae8b25a502a
Giang-Davy <davy.dangvu@gmail.com>
2025-08-07 20:56:51 +0000

    ajout du code 20

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,1 +1,17 @@
---
+-- Stocke et calcul la moyenne d'un etudiant
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE avg_score FLOAT;
+
+	SELECT AVG(score) INTO avg_score
+	FROM corrections
+	WHERE user_id = user_id;
+
+	UPDATE users
+	SET average_score = avg_score
+	WHERE id = user_id;
+END $$
+
+DELIMITER ;
