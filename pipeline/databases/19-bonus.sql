239930b0ef79cbed7a8f33a2b581ce9ef330e610
Giang-Davy <davy.dangvu@gmail.com>
2025-08-07 20:49:22 +0000

    correction code

--- pipeline/databases/19-bonus.sql
+++ pipeline/databases/19-bonus.sql
@@ -1,20 +1,17 @@
--- Ajoute une correction pour un �tudiant
+-- Ajoute une correction pour un e�udiant
 DELIMITER //
 
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
 	DECLARE project_id INT;
 
-	-- Vérifie si le projet existe déjà
 	SELECT id INTO project_id FROM projects WHERE name = project_name LIMIT 1;
 
-	-- Si le projet n'existe pas, on le crée
 	IF project_id IS NULL THEN
 		INSERT INTO projects (name) VALUES (project_name);
 		SET project_id = LAST_INSERT_ID();
 	END IF;
 
-	-- Ajoute la correction
 	INSERT INTO corrections (user_id, project_id, score)
 	VALUES (user_id, project_id, score);
 END;
