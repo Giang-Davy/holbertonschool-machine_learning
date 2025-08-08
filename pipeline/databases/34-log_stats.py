eca77f94b91498e3b44b25313586b73c3492928f
Giang-Davy <davy.dangvu@gmail.com>
2025-08-08 14:51:45 +0200

    pycode

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -7,22 +7,22 @@ from pymongo import MongoClient
 
 def log_stats():
     """donne des stats"""
-	client = MongoClient()
-	collection = client.logs.nginx
+    client = MongoClient()
+    collection = client.logs.nginx
 
-	count = collection.count_documents({})
-	print(f"{count} logs")
+    count = collection.count_documents({})
+    print(f"{count} logs")
 
-	print("Methods:")
-	for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-		n = collection.count_documents({"method": method})
-		print(f"\tmethod {method}: {n}")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        n = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {n}")
 
-	status_check = collection.count_documents(
+    status_check = collection.count_documents(
             {"method": "GET", "path": "/status"})
-	print(f"{status_check} status check")
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
     """main"""
-	log_stats()
+    log_stats()
