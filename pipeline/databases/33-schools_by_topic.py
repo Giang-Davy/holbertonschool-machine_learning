ed822377912db9441b4620b8d5dccf2aaef4d2ac
Giang-Davy <davy.dangvu@gmail.com>
2025-08-08 14:43:02 +0200

    ajout du code 33

---
+++ pipeline/databases/33-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""33-schools_by_topic.py"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """retourne une liste avec un sujet prÉcis"""
+	return list(mongo_collection.find({"topics": topic}))
